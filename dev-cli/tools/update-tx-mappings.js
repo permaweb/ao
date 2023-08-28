1fc225b125f48b1ed0561dfa5396dc5e3c5c242a
Tyler Hall <tyler@hyper.io>
2023-08-28 01:08:34 +0000

    chore(dev-cli): added crude mechanism for mapping version to transaction #14

---
+++ dev-cli/tools/update-tx-mappings.js
@@ -0,0 +1,52 @@
+/**
+ * USAGE:
+ *
+ * deno run -A update-tx-mappings.js [-- <SCRIPT_ARG>]
+ *
+ * OPTIONS:
+ *
+ * The transaction id that contains the binaries
+ * --binaries=SYHBhGAmBo6fgAkINNoRtumOzxNB8-JFv2tPhBuNk5c
+ *
+ * The transaction id that contains the install script
+ * --install=F63wJCavB_sN2xxW-qtQ1Vv7_eRgmYCdcoQPMp_-N0w
+ *
+ * The semver version
+ * --version=1.2.3
+ *
+ * Whether these transaction ids should be used as the latest version in the manifest
+ * --latest
+ */
+import { parse } from "https://deno.land/std@0.200.0/flags/mod.ts";
+import { join } from "https://deno.land/std@0.200.0/path/mod.ts";
+
+import manifest from "../egg.json" assert { type: "json" };
+
+async function main() {
+  const { binaries, install, version, latest } = parse(Deno.args);
+
+  if (!binaries || !install || !version) {
+    throw new Error(
+      'Insufficient arguments. "--binaries", "--install", and "--version" are required',
+    );
+  }
+
+  manifest.version = version
+
+  console.log(`Writing new txMappings for version ${version}...`);
+  manifest.txMappings.binaries[version] = binaries;
+  manifest.txMappings.install[version] = install;
+
+  if (latest) {
+    console.log(`Updating latest txMappings to version ${version}...`);
+    manifest.txMappings.binaries.latest = binaries;
+    manifest.txMappings.install.latest = install;
+  }
+
+  const here = new URL(import.meta.url).pathname;
+  const dest = join(here, "../..", "egg.json");
+
+  await Deno.writeTextFile(dest, JSON.stringify(manifest, null, 2));
+}
+
+await main();
