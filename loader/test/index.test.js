5e205bd05cb4f23f31373170a9ecf422554818c4
Tyler Hall <tyler@hyper.io>
2023-09-04 14:38:48 +0000

    refactor(loader): rename to ao-loader and update README #18

--- loader/test/index.test.js
+++ loader/test/index.test.js
@@ -1,46 +1,46 @@
-import { test } from 'uvu'
-import * as assert from 'uvu/assert'
+import { describe, it } from 'node:test'
+import * as assert from 'node:assert'
 
 import hyperbeamLoader from '../src/index.cjs'
 import fs from 'fs'
 
-test('load and execute lua contract', async () => {
-  const wasmBinary = fs.readFileSync('./test/contract.wasm')
-  const handle = hyperbeamLoader(wasmBinary)
-  const result = await handle({ balances: { '1': 1 } }, { caller: '1', input: { function: 'balance' } }, {})
-  console.log(result)
-  assert.ok(true)
-})
+describe('loader', () => {
+  it('load and execute lua contract', async () => {
+    const wasmBinary = fs.readFileSync('./test/contract.wasm')
+    const handle = hyperbeamLoader(wasmBinary)
+    const result = await handle({ balances: { '1': 1 } }, { caller: '1', input: { function: 'balance' } }, {})
+    console.log(result)
+    assert.ok(true)
+  })
 
-test('load and execute message passing contract', async () => {
-  const wasmBinary = fs.readFileSync('./test/contract-message/contract.wasm')
-  const mainHandler = hyperbeamLoader(wasmBinary)
-  const mainResult = await mainHandler(
-    {
-      balances: { '1': 1 },
-      sendToContract: 'ctr-id-123'
-    },
-    {
-      input: { function: "noop" }
-    },
-    {
-      transaction: { id: 'tx-id-123' },
-      contract: { id: 'ctr-id-456' }
-    }
-  )
-  console.log(mainResult.result)
-  const { result: { messages: [message] } } = mainResult
-  assert.equal(message, {
-    target: 'ctr-id-123',
-    txId: 'tx-id-123',
-    message: {
-      caller: 'ctr-id-456',
-      qty: 10,
-      type: 'transfer',
-      from: 'ctr-id-456',
-      to: 'ctr-id-123'
-    }
+  it('load and execute message passing contract', async () => {
+    const wasmBinary = fs.readFileSync('./test/contract-message/contract.wasm')
+    const mainHandler = hyperbeamLoader(wasmBinary)
+    const mainResult = await mainHandler(
+      {
+        balances: { '1': 1 },
+        sendToContract: 'ctr-id-123'
+      },
+      {
+        input: { function: "noop" }
+      },
+      {
+        transaction: { id: 'tx-id-123' },
+        contract: { id: 'ctr-id-456' }
+      }
+    )
+    console.log(mainResult.result)
+    const { result: { messages: [message] } } = mainResult
+    assert.deepStrictEqual(message, {
+      target: 'ctr-id-123',
+      txId: 'tx-id-123',
+      message: {
+        caller: 'ctr-id-456',
+        qty: 10,
+        type: 'transfer',
+        from: 'ctr-id-456',
+        to: 'ctr-id-123'
+      }
+    })
   })
 })
-
-test.run()
