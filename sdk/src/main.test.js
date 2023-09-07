397fa2a87f3080e174314fc8d31edcc52dfda754
Tyler Hall <tyler@hyper.io>
2023-09-07 23:09:47 +0000

    refactor(sdk): to inject dal #20

--- sdk/src/main.test.js
+++ sdk/src/main.test.js
@@ -1,11 +1,12 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 
+const GATEWAY_URL = globalThis.GATEWAY || "https://arweave.net";
 const CONTRACT = "zc24Wpv_i6NNCEdxeKt7dcNrqL5w0hrShtSCcFGGL24";
 
 test("readState", async () => {
   const { readStateWith } = await import("./main.js");
-  const result = await readStateWith({ fetch })(CONTRACT);
+  const result = await readStateWith({ fetch, GATEWAY_URL })(CONTRACT);
   console.log(result);
   assert.ok(true);
 });
