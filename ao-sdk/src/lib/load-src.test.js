5dbe085f0d3e24ba042c507a7d5931ac6f2f2e96
Tyler Hall <tyler@hyper.io>
2023-09-07 17:53:46 +0000

    style(ao-sdk): deno fmt

--- ao-sdk/src/lib/load-src.test.js
+++ ao-sdk/src/lib/load-src.test.js
@@ -1,11 +1,11 @@
-import test from 'node:test'
-import assert from 'node:assert/strict'
+import test from "node:test";
+import assert from "node:assert/strict";
 
-const CONTRACT = 'zc24Wpv_i6NNCEdxeKt7dcNrqL5w0hrShtSCcFGGL24'
+const CONTRACT = "zc24Wpv_i6NNCEdxeKt7dcNrqL5w0hrShtSCcFGGL24";
 
-test('load contract source', async () => {
-  const { loadSource } = await import('./load-src.js')
-  const result = await loadSource({id: CONTRACT}).toPromise()
-  assert.ok(result.src.byteLength === 214390)
-  assert.ok(true)
-})
+test("load contract source", async () => {
+  const { loadSource } = await import("./load-src.js");
+  const result = await loadSource({ id: CONTRACT }).toPromise();
+  assert.ok(result.src.byteLength === 214390);
+  assert.ok(true);
+});
