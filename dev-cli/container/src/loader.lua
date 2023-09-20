b06346c4aa44f270e137d05aa42902ba3016b785
Tyler Hall <tyler@hyper.io>
2023-09-20 16:55:06 +0000

    feat(dev-cli): loader.lua returns consistent shape and catches errors from contract handle #49

--- dev-cli/container/src/loader.lua
+++ dev-cli/container/src/loader.lua
@@ -1,14 +1,26 @@
 local json = require "json"
 local contract = require ".src.contract"
 
-function handle(stateJSON, actionJSON, SmartWeaveJSON) 
+function handle(stateJSON, actionJSON, SmartWeaveJSON)
   -- decode inputs
   local state = json.decode(stateJSON)
   local action = json.decode(actionJSON)
   local SmartWeave = json.decode(SmartWeaveJSON)
+
   -- handle contract
-  local response = contract.handle(state, action, SmartWeave)
+  --
+  -- The contract may throw an error, either intentionally or unintentionally
+  -- So we need to be able to catch these unhandled errors and bubble them
+  -- across the interop with some indication that it was unhandled
+  --
+  -- To do this, we wrap the contract.handle with pcall(), and return both the status
+  -- and response as JSON. The caller can examine the status boolean and decide how to
+  -- handle the error
+  --
+  -- See pcall https://www.lua.org/pil/8.4.html
+  local status, response = pcall(function() return (contract.handle(state, action, SmartWeave)) end)
+
   -- encode output
-  local responseJSON = json.encode(response)
+  local responseJSON = json.encode({ ok = status, response = response })
   return responseJSON
 end
