2a46787c8120f972336ece117ea87dd3b895730d
Justin Shaw <mogulx_operates@yahoo.com>
2023-09-27 08:17:13 -0500

    chore: run tests with my local

--- servers/testscripts/src/test-ao.js
+++ servers/testscripts/src/test-ao.js
@@ -1,40 +1,31 @@
+const fs = require("fs");
+const path = require("path");
 
-
-
-
-const fs = require('fs');
-const path = require('path');
-
-const { ArweaveSigner } = require('warp-arbundles');
+const { ArweaveSigner } = require("warp-arbundles");
 
 globalThis.MU_URL = "http://localhost:3004";
 globalThis.CU_URL = "http://localhost:3005";
-const { writeInteraction, readState } = require('@permaweb/ao-sdk');
+const { writeInteraction, readState } = require("@permaweb/ao-sdk");
 
-const CONTRACT_TX_ID = "4ILANxSL3P4RtbbVamYynK6YLsPNUJ4WX1nYziOB0v4";
+const CONTRACT_TX_ID = "ECzTzsbsvo_Mbc_54Nn7I6hIeobyvIStdi92r0HPYug";
 (async function () {
-    console.log('Testing ao...');
+  console.log("Testing ao...");
 
-    const walletPath = process.env.PATH_TO_WALLET;
+  const walletPath = process.env.PATH_TO_WALLET;
 
-    let walletKey = JSON.parse(fs.readFileSync(path.resolve(walletPath), 'utf8'));
-    let signer = new ArweaveSigner(walletKey);
+  let walletKey = JSON.parse(fs.readFileSync(path.resolve(walletPath), "utf8"));
+  let signer = new ArweaveSigner(walletKey);
 
-    let input = { function: 'noop' };
-    let tags = [];
+  let input = { function: "noop" };
+  let tags = [];
 
-    let s = await readState(CONTRACT_TX_ID);
-    console.log(s)
+  let s = await readState(CONTRACT_TX_ID);
+  console.log(s);
 
-    await writeInteraction(
-        CONTRACT_TX_ID, 
-        input, 
-        signer, 
-        tags
-    );
+  await writeInteraction(CONTRACT_TX_ID, input, signer, tags);
 
-    // let newState = await fetch(`${CU_URL}${CONTRACT_ENDPOINT}${CONTRACT_TX_ID}`);
-    // let newStateJson = await newState.json();
+  // let newState = await fetch(`${CU_URL}${CONTRACT_ENDPOINT}${CONTRACT_TX_ID}`);
+  // let newStateJson = await newState.json();
 
-    // console.log(newStateJson);
+  // console.log(newStateJson);
 })();
