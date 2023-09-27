7848d004f552c81576ab718341b81aa933c5c44f
Justin Shaw <mogulx_operates@yahoo.com>
2023-09-27 09:07:33 -0500

    refactor: import something; add sum func

--- lua-examples/counter/contract.lua
+++ lua-examples/counter/contract.lua
@@ -1,13 +1,21 @@
 
 local contract = { _version = "0.0.1" }
+local API = require('api.index')
 
-function contract.handle(state, action, SmartWeave) 
+
+function contract.handle(state, action, SmartWeave)
   -- do stuff
+  API.send();
+  print('test')
   local response = {
     state = state,
     result = { messages = {} }
   }
   return response
+end
+
+function contract.sum(num)
+  return num * 2
 end
 
 return contract
