3e40caefc1c2ba45b4c71d4141d889b8bcf0855d
Justin Shaw <mogulx_operates@yahoo.com>
2023-09-28 17:34:56 -0500

    refactor: add api and cases

--- lua-examples/sw-token/contract.lua
+++ lua-examples/sw-token/contract.lua
@@ -1,13 +1,17 @@
+local contract = {
+  _version = "0.0.1"
+}
 
-local contract = { _version = "0.0.1" }
+local API = require('api.index')
 
-function contract.handle(state, action, SmartWeave) 
-  -- do stuff
-  local response = {
-    state = state,
-    result = { messages = {} }
+function contract.handle(state, action, SmartWeave)
+  local cases = {
+    mint = 'mint',
+    transfer = 'transfer',
+    balance = 'balance'
   }
-  return response
+  local funk = cases[action.input['function']] or 'default'
+  return API[funk](state, action, SmartWeave)
 end
 
 return contract
