254f683165571596dd10b37a1ceee6d315ae3db7
Tyler Hall <tyler@hyper.io>
2023-10-05 22:08:58 +0000

    feat(sdk): add wallet api on node createDataItemSigner impl

--- sdk/src/client/node/wallet.js
+++ sdk/src/client/node/wallet.js
@@ -10,7 +10,7 @@ const { createData, ArweaveSigner } = WarpArBundles
  * to use, but consumers can also implement their own signer
  */
 export function createDataItemSigner (wallet) {
-  return async ({ data, tags }) => {
+  const signer = async ({ data, tags }) => {
     const signer = new ArweaveSigner(wallet)
     const dataItem = createData(data, signer, { tags })
     return dataItem.sign(signer)
@@ -19,4 +19,16 @@ export function createDataItemSigner (wallet) {
         raw: await dataItem.getRaw()
       }))
   }
+
+  /**
+   * expose the raw wallet on the signer function,
+   * in the case it is needed by a dependency that handles
+   * its signing internally ie. the irys SDK
+   *
+   * We wrap in a function, so it is not printed as part of
+   * debug logs
+   */
+  signer._ = () => wallet
+
+  return signer
 }
