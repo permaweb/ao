4c46865b2bea0968be1b60c3e8e7473fa9c4035a
Justin Shaw <mogulx_operates@yahoo.com>
2023-10-06 15:49:08 -0500

    refactor: add printTable

--- lua-examples/message-passing/sender/contract.lua
+++ lua-examples/message-passing/sender/contract.lua
@@ -1,11 +1,30 @@
+local contract = {
+  _version = "0.0.1"
+}
 
-local contract = { _version = "0.0.1" }
+local function printTable(table, indent)
+  indent = indent or 0
 
-function contract.handle(state, action, SmartWeave) 
+  for k, v in pairs(table) do
+    if type(v) == "table" then
+      print(string.rep("  ", indent) .. k .. " = {")
+      printTable(v, indent + 1)
+      print(string.rep("  ", indent) .. "}")
+    else
+      print(string.rep("  ", indent) .. k .. " = " .. tostring(v))
+    end
+  end
+end
+
+function contract.handle(state, action, SmartWeave)
+  printTable(SmartWeave)
+
   -- do stuff
   local response = {
     state = state,
-    result = { messages = {} }
+    result = {
+      messages = {}
+    }
   }
   return response
 end
