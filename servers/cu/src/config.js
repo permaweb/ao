e5eb8474b13fc73eacf23da0b03ee3ed75272742
Justin Shaw <mogulx_operates@yahoo.com>
2023-10-09 14:35:37 -0500

    fix: revert config

--- servers/cu/src/config.js
+++ servers/cu/src/config.js
@@ -1,12 +1,13 @@
-if (!process.env.WALLET) {
-  console.error(
-    'Please pass a WALLET into the environment. eg. process.env.WALLET should be your JWK string.'
-  )
-}
+import path from 'path'
+import fs from 'fs'
+
+const walletPath = process.env.PATH_TO_WALLET
+const walletKey = JSON.parse(fs.readFileSync(path.resolve(walletPath), 'utf8'))
 
 const config = {
-  muWallet: JSON.parse(process.env.WALLET || ''),
+  muWallet: walletKey,
   sequencerUrl: 'https://gw.warp.cc',
+  cuUrl: process.env.CU_URL,
   port: 3005
 }
 
