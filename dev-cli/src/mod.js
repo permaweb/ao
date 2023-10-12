d757c5c564ef56dde98679b17d37b19655b8ce98
Tyler Hall <tyler@hyper.io>
2023-10-12 21:07:14 +0000

    style(dev-cli): mod.js file since no longer importing manifest as json

--- dev-cli/src/mod.js
+++ dev-cli/src/mod.js
@@ -1,26 +1,28 @@
-import { Command } from "./deps.js"
+/* global Deno */
 
-import { VERSION } from "./versions.js"
+import { Command } from './deps.js'
 
-import { command as Init } from "./commands/init.js"
-import { command as Repl } from "./commands/repl.js"
-import { command as Run } from "./commands/run.js"
-import { command as Build } from "./commands/build.js"
-import { command as Publish } from "./commands/publish.js"
+import { VERSION } from './versions.js'
+
+import { command as Init } from './commands/init.js'
+import { command as Repl } from './commands/repl.js'
+import { command as Run } from './commands/run.js'
+import { command as Build } from './commands/build.js'
+import { command as Publish } from './commands/publish.js'
 import { command as Contract } from './commands/contract.js'
-import { command as Bundler } from "./commands/bundler.js"
+import { command as Bundler } from './commands/bundler.js'
 
 const cli = new Command()
-  .name("ao")
+  .name('ao')
   .version(VERSION.CLI)
-  .description("The ao CLI for building and publishing ao Processes")
+  .description('The ao CLI for building and publishing ao Processes')
   .action(() => cli.showHelp())
-  .command("init", Init)
-  .command("repl", Repl)
-  .command("run", Run)
-  .command("build", Build)
-  .command("publish", Publish)
-  .command("contract", Contract)
-  .command("bundler", Bundler)
+  .command('init', Init)
+  .command('repl', Repl)
+  .command('run', Run)
+  .command('build', Build)
+  .command('publish', Publish)
+  .command('contract', Contract)
+  .command('bundler', Bundler)
 
 await cli.parse(Deno.args)
