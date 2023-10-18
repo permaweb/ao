23d7ef6978dd283dc5472441418e4cdd6b256b69
Tyler Hall <tyler@hyper.io>
2023-10-18 22:31:41 +0000

    feat(cu): expose scheduled endpoint #98

--- servers/cu/src/routes/index.js
+++ servers/cu/src/routes/index.js
@@ -2,8 +2,10 @@ import { pipe } from 'ramda'
 
 import { withStateRoutes } from './state.js'
 import { withResultRoutes } from './result.js'
+import { withScheduledRoutes } from './scheduled.js'
 
 export const withRoutes = pipe(
   withStateRoutes,
-  withResultRoutes
+  withResultRoutes,
+  withScheduledRoutes
 )
---
+++ servers/cu/src/routes/scheduled.js
@@ -0,0 +1,31 @@
+import { always, compose } from 'ramda'
+
+import { withMiddleware } from './middleware/index.js'
+import { z } from 'zod'
+
+const inputSchema = z.object({
+  processId: z.string().min(1, 'an ao process id is required'),
+  from: z.string().optional(),
+  to: z.string().optional()
+})
+
+export const withScheduledRoutes = app => {
+  app.get(
+    '/scheduled/:processId',
+    compose(
+      withMiddleware,
+      always(async (req, res) => {
+        const {
+          params: { processId },
+          query: { from, to },
+          domain: { apis: { readScheduledMessages } }
+        } = req
+
+        const input = inputSchema.parse({ processId, from, to })
+        return res.send(await readScheduledMessages(input))
+      })
+    )()
+  )
+
+  return app
+}
