b47826ab70607d7f6a6851455e52f2395b018bdb
Tyler Hall <tyler@hyper.io>
2023-11-08 18:45:58 +0000

    refactor(lua-examples): use bibo process code and multi-file module lua

---
+++ lua-examples/process/process.lua
@@ -0,0 +1,97 @@
+local JSON = require("json")
+local ao = require('.src.ao')
+
+local process = { _version = "0.0.6" }
+
+local function findObject(array, key, value)
+  for i, object in ipairs(array) do
+    if object[key] == value then
+      return object
+    end
+  end
+  return nil
+end
+
+function initializeState(msg, env)
+  if not owner then
+    owner = env.process.owner
+  end
+
+  if not inbox then
+    inbox = {}
+  end
+
+  if not name then
+    local aosName = findObject(msg.tags, "name", "name")
+    if aosName then
+      name = aosName.value
+    else
+      name = 'aos'
+    end
+  end
+
+  if not prompt then
+    local promptObject = findObject(msg.tags, "name", "prompt")
+    if promptObject then
+      prompt = promptObject.value
+    else
+      prompt = 'aos'
+    end
+  end
+end
+
+function version()
+  print("version: " .. process._version)
+end
+
+function process.handle(msg, env)
+  initializeState(msg, env)
+
+  local fn = findObject(msg.tags, "name", "function")
+
+  if fn ~= nil and fn.value == "eval" and owner == msg.owner then
+    local messages = {}
+    local spawns = {}
+    local env = {
+      _global = _G,
+      state = state
+    }
+
+    function env.send(target, input)
+      local message = ao.send(input, target, AO)
+      table.insert(messages, message)
+      return 'message added to outbox'
+    end
+
+    function env.spawn(data, input)
+      local spawn = ao.spawn(data, input, AO)
+      table.insert(spawns, spawn)
+      return 'spawn process request'
+    end
+
+    -- exec expression
+    local expr = findObject(msg.tags, "name", "expression")
+    if (expr == nil) then return { result = { error = "could not find expression" } } end
+
+    local func, err = load(expr.value, 'aos', 't', _G)
+    local output = ""
+    if func then
+      output, e = func()
+    else
+      output = err
+    end
+    if e then output = e end
+
+    return { output = { data = { output = output, prompt = prompt } }, messages = messages, spawns = spawns }
+  else
+    -- Add Message to Inbox
+  end
+
+
+  local response = {
+    result = { error = "could not find action" }
+  }
+  return response
+end
+
+return process
