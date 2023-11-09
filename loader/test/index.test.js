410de195bd22227716bf67699728dc3e6e498f66
Tyler Hall <tyler@hyper.io>
2023-11-09 13:44:56 +0000

    test(loader): cleaned up awaits and naming in test

--- loader/test/index.test.js
+++ loader/test/index.test.js
@@ -12,11 +12,11 @@ console.log(`${MODULE_PATH}`)
 
 describe('loader', async () => {
   it('load and execute message passing contract', async () => {
-    const { default: hyperbeamLoader } = await import(MODULE_PATH)
+    const { default: AoLoader } = await import(MODULE_PATH)
 
     const wasmBinary = fs.readFileSync('./test/contracts/process.wasm')
-    const mainHandler = await hyperbeamLoader(wasmBinary)
-    const mainResult = await mainHandler(
+    const mainHandler = await AoLoader(wasmBinary)
+    const mainResult = mainHandler(
       null,
       {
         owner: 'tom',
@@ -36,16 +36,16 @@ describe('loader', async () => {
   })
 
   it('should load previous memory', async () => {
-    const { default: hyperbeamLoader } = await import(MODULE_PATH)
+    const { default: AoLoader } = await import(MODULE_PATH)
 
     const wasmBinary = fs.readFileSync('./test/contracts/process2.wasm')
-    const mainHandler = await hyperbeamLoader(wasmBinary)
+    const mainHandler = await AoLoader(wasmBinary)
     // spawn
-    const result = await mainHandler(null, { owner: 'tom', tags: [{ name: 'function', value: 'count' }] }, {})
+    const result = mainHandler(null, { owner: 'tom', tags: [{ name: 'function', value: 'count' }] }, {})
     assert.equal(result.output, 'count: 1')
 
-    const nextHandler = await hyperbeamLoader(wasmBinary)
-    const result2 = await nextHandler(result.buffer, { owner: 'tom', tags: [{ name: 'function', value: 'count' }] }, {})
+    const nextHandler = await AoLoader(wasmBinary)
+    const result2 = nextHandler(result.buffer, { owner: 'tom', tags: [{ name: 'function', value: 'count' }] }, {})
     assert.equal(result2.output, 'count: 2')
     assert.ok(true)
   })
