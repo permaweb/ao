0b80ae38e8004b4640d557b96e530a98630454cc
Vince Juliano <v.juliano6363@gmail.com>
2023-11-13 18:40:14 -0500

    feat(mu) spawn success msg

--- servers/mu/src/domain/lib/processSpawn/spawn-process.js
+++ servers/mu/src/domain/lib/processSpawn/spawn-process.js
@@ -6,25 +6,25 @@ const ctxSchema = z.object({
   contractTx: z.any()
 }).passthrough()
 
-export function createContractWith (env) {
-  const { logger, writeContractTx } = env
+export function spawnProcessWith (env) {
+  const { logger, writeProcessTx } = env
 
   return (ctx) => {
     const { initState, src, tags } = ctx.cachedSpawn.spawn
     const transformedData = { initState, src, tags }
 
     return of(transformedData)
-      .chain(fromPromise(writeContractTx))
+      .chain(fromPromise(writeProcessTx))
       .bichain(
         (error) => {
-          console.error('writeContractTx failed. Recovering and returning original ctx.', error)
+          console.error('writeProcessTx failed. Recovering and returning original ctx.', error)
           return of(ctx)
         },
         (result) => {
           return of(result)
-            .map(assoc('contractTx', __, ctx))
+            .map(assoc('processTx', __, ctx))
             .map(ctxSchema.parse)
-            .map(logger.tap('Added "contractTx" to ctx'))
+            .map(logger.tap('Added "processTx" to ctx'))
         }
       )
   }
