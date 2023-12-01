9ce290349f99146a5766234b76e9c5242ad9e97d
Vince Juliano <v.juliano6363@gmail.com>
2023-11-30 21:48:43 -0500

    chore(su) core unit tests

--- servers/su/src/domain/core/builder.rs
+++ servers/su/src/domain/core/builder.rs
@@ -14,6 +14,7 @@ pub struct Builder {
     signer: Arc<dyn Signer>
 }
 
+
 pub struct BuildResult{
     pub binary: Vec<u8>,
     pub bundle: DataBundle
@@ -79,6 +80,7 @@ impl Builder {
         let mut data_bundle = DataBundle::new(sort_key.clone());
         data_bundle.add_item(item);
         let buffer = data_bundle.to_bytes()?;
+        
         let process_id = data_bundle.items[0].target().clone();
 
         let tags = vec![
@@ -102,5 +104,76 @@ impl Builder {
             binary: new_data_item.as_bytes()?,
             bundle: data_bundle
         })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_trait::async_trait;
+    use std::sync::Arc;
+    use crate::domain::core::dal::NetworkInfo;
+
+    struct MockGateway;
+    #[async_trait]
+    impl Gateway for MockGateway {
+        async fn check_head(&self, _tx_id: String) -> Result<bool, String> {
+            Ok(true)
+        }
+
+        async fn network_info(&self) -> Result<NetworkInfo, String> {
+            Ok(NetworkInfo { 
+                height: "1000".to_string(), 
+                current: "test-network".to_string() 
+            })
+        }
+    }
+
+    struct MockWallet;
+    impl Wallet for MockWallet {
+        fn wallet_json(&self) -> Result<String, String> {
+            let jwk = r#"{
+                "kty": "RSA",
+                "e": "AQAB",
+                "n": "asdf",
+                "d": "adsf",
+                "p": "asdf",
+                "q": "asdf",
+                "dp": "adsf",
+                "dq": "adsf",
+                "qi": "asdf"
+            }"#;
+
+            Ok(jwk.to_string())
+        }
+    }
+
+    struct MockSigner;
+    #[async_trait]
+    impl Signer for MockSigner {
+        async fn sign_tx(&self, _buffer: Vec<u8>) -> Result<Vec<u8>, String> {
+            Ok(vec![1, 2, 3, 4]) 
+        }
+
+        fn get_public_key(&self) -> Vec<u8> {
+            vec![5, 6, 7, 8]
+        }
+    }
+
+    #[tokio::test]
+    async fn test_build_success() {
+        let gateway = Arc::new(MockGateway);
+        let wallet = Arc::new(MockWallet);
+        let signer = Arc::new(MockSigner);
+
+        let builder = Builder::new(gateway, wallet, signer)
+            .expect("Failed to create Builder");
+
+        let tx = base64_url::decode(&"AQB9q2yhsQlBHv2LOTIrtmKjw063S1DG0prKcq86DykIegmPnXOReXkWXwpqXt4YxTRw6Rw1jG7f1QFF5ReoJO2MrJmia9ymkTmnhamv3lsYYIotBC6U4Bmzo6IZiKmn2llJt0MDvCe8rxzG15vvff9bpnDIVflY_Dm9Y0dCH-w2Xg8rb2xLq-cM8SBoNRiYruwcwpahiHTjXcxboJKksZRXaI_E7_7vL1gWlMLqeYeF_uXqkth8_PGtZcqMA7pbTYcRzGki_rifGXKUIZKgSIRXTk54iboiqNzOklIFpDKDJpC9Xk_6ppSw_Xzs8S0KpR-veBL8TeURtGhrsDecu_36Pk2MMvdZedxiAg7bvQ9H_NZecoZcju-sQKZiE7haq9Nos3g6njh9IpXivGJ1k8tRLeox7hXOeynffzcXz1Vnz5c4Zxw8LKUbLygni49sflKyFTMnQ8sgDw00fPsuhrznq37-2OLhmYe-tIg-TEV3T4VNdqchzeRSFIv_l7ZJcxeFxcEgdq9aXMx2yzVhSInFuk_W8fJSbhPKX9cewbr4BA_XUNMReowLVcnjB_19iCWnivkVk9sz-QRbjuVL2IMqZePWcRdN5ncXRJoYv4F-Z4FfXDCFuyCD4UAtiQfdch-S4KvRf99DwKrZrMIF28MDdRFdE3ZGDs3FXcPuN8eMLoKBrkyfkM3J89W1GNvrcCNHSNzhF8oPItU4Qno7-x52ZIOAjfdFcXTYLQYU7Xfr6GKaRByemPrkbkrJpdB8RQREt3rQRDNGRQ0jnbPn62PQugvss98JZn9D4ScNusbbgKMihj4MqfXE2mt7Ab9ewx5d01d-Mwf3D6mGz_ERBJgJo8b119bRXdNvgUDJC58NFd4chEOUF4mbyj2pZB9P7fx22yEvV7y6DNzuKvk02YQt7TwL7sdxH1PT63CYJx0tlVGGDvJhGKUQwOfDaXHFMjuuUlXa_klTJT5wEb78aAyh33rw0n9wpOakTIk2KgekbJAzVWCT0BfLrrOhKs3556_d--2mLmcLOONosBjSLokuvtyrTOX7btKRf6Zl5l3wtxsFaPgO6M3Qy9UR46AtK76XSFQd9kcDf_Qj1FyronJS_enQFWYn5Um97mDnYT9SJwMpDFS_FYBTKlsNhsVy11EW5kKuo6mTRlfebJa9CQv-NzbUajd7ulAcM4VNWYt-KbbhVZtUUUxgDvXJdlwRSYR5U8JwSze3sfatb5mbds-EAS-tT7grwrvTb4wRz20e9ARtBg6kC_x8QujHmFORJ97zrFlnnunPbsWgwWz8bfT9RMFy5xUE1KDCtnJqp-M3FoWwQc4sREIyCl7Q6JTq_slPe-Xwt9C5oquj4e_SoOuTAfqDPAmIG6rEXKSN7RP3KRjN5IA5Wpp2I0hgOJ6bT2qNAAUAAAAAAAAASAAAAAAAAAAKGkRhdGEtUHJvdG9jb2wEYW8QZnVuY3Rpb24GcmF3GkRhdGEtUHJvdG9jb2wEYW8OYW8tdHlwZQ5tZXNzYWdlBlNESwRhbwA2NTgz".to_string()).expect("failed to encode data item");
+
+        let result = builder.build(tx).await;
+
+        assert!(result.is_ok());
     }
 }
