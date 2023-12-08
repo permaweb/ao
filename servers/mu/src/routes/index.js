287f516ee91cfc3104b255389d001e3c57869db5
Tyler Hall <tyler@hyper.io>
2023-12-08 17:20:04 +0000

    feat(mu): trace endpoint #188

--- servers/mu/src/routes/index.js
+++ servers/mu/src/routes/index.js
@@ -3,8 +3,10 @@ import { pipe } from 'ramda'
 import { withMessageRoutes } from './message.js'
 import { withSpawnRoutes } from './spawn.js'
 import { withMonitorRoutes } from './monitor.js'
+import { withTraceRoutes } from './trace.js'
 
 export const withRoutes = pipe(
+  withTraceRoutes,
   withMessageRoutes,
   withSpawnRoutes,
   withMonitorRoutes
---
+++ servers/mu/src/routes/trace.js
@@ -0,0 +1,77 @@
+import { always, compose } from 'ramda'
+import { of } from 'hyper-async'
+import { z } from 'zod'
+
+import { withMiddleware } from './middleware/index.js'
+
+/**
+ * TODO: could be moved into a route utils or middleware
+ *
+ * keeping local for now, for simplicity
+ */
+const toConnection = ({ cursorFn }) => ({ nodes, pageSize }) => {
+  return {
+    pageInfo: {
+      hasNextPage: nodes.length > pageSize
+    },
+    edges: nodes.slice(0, pageSize).map(node => ({
+      node,
+      cursor: cursorFn(node)
+    }))
+  }
+}
+
+export const withTraceRoutes = (app) => {
+  const DEFAULT_PAGE_SIZE = 10
+  const traceConnection = toConnection({ cursorFn: (trace) => trace.id })
+
+  app.get(
+    '/',
+    compose(
+      withMiddleware,
+      always(async (req, res) => {
+        const {
+          query,
+          logger: _logger,
+          domain: { apis: { traceMsgs } }
+        } = req
+
+        /**
+         * Not debugging, so just return the base response
+         */
+        if (!query.debug) return res.send('ao messenger unit')
+
+        const logger = _logger.child('GET_TRACE')
+
+        const inputSchema = z.object({
+          process: z.string().optional(),
+          message: z.string().optional(),
+          page: z.coerce(z.number().int()).default(1),
+          pageSize: z.coerce(z.number().int()).default(DEFAULT_PAGE_SIZE)
+        })
+
+        const input = await inputSchema.parseAsync(query)
+
+        if (input.process && input.message) {
+          const err = new Error('Only one of process or message query parameters can be provided')
+          err.status = 422
+          throw err
+        }
+
+        /**
+         * Retrieve all message traces for the given input
+         */
+        await of({ ...input, limit: input.pageSize, offset: input.page })
+          .chain(traceMsgs)
+          .bimap(
+            logger.tap('Failed to retrieve trace for process "%s" or message "%s"', input.process, input.message),
+            logger.tap('Successfully retrieved trace for process "%s" or message "%s"', input.process, input.message)
+          )
+          .map(({ traces }) => res.send(traceConnection({ nodes: traces, pageSize: input.pageSize })))
+          .toPromise()
+      })
+    )()
+  )
+
+  return app
+}
