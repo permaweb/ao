9282ec12e1dc4aaa78bfcf394ec836efe9988b8f
Tyler Hall <tyler@hyper.io>
2023-12-17 22:44:00 +0000

    refactor(cu): remove in memory cache from state endpoint

--- servers/cu/src/routes/state.js
+++ servers/cu/src/routes/state.js
@@ -1,8 +1,7 @@
-import { compose } from 'ramda'
+import { always, compose } from 'ramda'
 import { z } from 'zod'
 
 import { withMiddleware } from './middleware/index.js'
-import { withInMemoryCache } from './middleware/withInMemoryCache.js'
 
 const inputSchema = z.object({
   processId: z.string().min(1, 'an ao process id is required'),
@@ -15,44 +14,28 @@ export const withStateRoutes = (app) => {
     '/state/:processId',
     compose(
       withMiddleware,
-      withInMemoryCache({
-        keyer: (req) => {
-          const {
-            params: { processId },
-            query: { to }
-          } = req
-          return `${processId}${to}`
-        },
-        loader: async (requests) => {
-          return requests.map(async ({ req, res }) => {
-            const {
-              params: { processId },
-              query: { to },
-              domain: { apis: { readState } }
-            } = req
+      always(async (req, res) => {
+        const {
+          params: { processId },
+          query: { to },
+          domain: { apis: { readState } }
+        } = req
 
-            const input = inputSchema.parse({ processId, to })
+        const input = inputSchema.parse({ processId, to })
 
-            return readState(input)
-              .map((output) => {
-                /**
-                 * The cu sends the array buffer as binary data,
-                 * so make sure to set the header to indicate such
-                 *
-                 * and then return only the buffer received from BL
-                 */
-                res.header('Content-Type', 'application/octet-stream')
-                return output.Memory
-              })
-              .toPromise()
-              /**
-               * Will bubble up to the individual load call
-               * on the dataloader, where it can be handled
-               * individually
-               */
-              .catch(err => err)
+        return readState(input)
+          .map((output) => {
+            /**
+             * The cu sends the array buffer as binary data,
+             * so make sure to set the header to indicate such
+             *
+             * and then return only the buffer received from BL
+             */
+            res.header('Content-Type', 'application/octet-stream')
+            return output.Memory
           })
-        }
+          .map((memory) => res.send(memory))
+          .toPromise()
       })
     )()
   )
