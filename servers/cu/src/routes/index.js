aa1ef5fa7652c16e54afd2dd32685ef5e7659191
Tyler Hall <tyler@hyper.io>
2023-12-17 21:51:50 +0000

    fix(cu): rename import

--- servers/cu/src/routes/index.js
+++ servers/cu/src/routes/index.js
@@ -2,10 +2,10 @@ import { pipe } from 'ramda'
 
 import { withStateRoutes } from './state.js'
 import { withResultRoutes } from './result.js'
-import { withScheduledRoutes } from './scheduled.js'
+import { withCronRoutes } from './cron.js'
 
 export const withRoutes = pipe(
   withStateRoutes,
   withResultRoutes,
-  withScheduledRoutes
+  withCronRoutes
 )
