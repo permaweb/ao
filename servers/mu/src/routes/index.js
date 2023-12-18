edf4ad5458850e8e447d5d2dae5c3ae5d8788bb8
Tyler Hall <tyler@hyper.io>
2023-12-18 21:39:48 +0000

    refactor(mu): combine root routes into single file and compose #244

--- servers/mu/src/routes/index.js
+++ servers/mu/src/routes/index.js
@@ -1,13 +1,9 @@
 import { pipe } from 'ramda'
 
 import { withRootRoutes } from './root.js'
-import { withSpawnRoutes } from './spawn.js'
 import { withMonitorRoutes } from './monitor.js'
-import { withTraceRoutes } from './trace.js'
 
 export const withRoutes = pipe(
   withRootRoutes,
-  withTraceRoutes,
-  withSpawnRoutes,
   withMonitorRoutes
 )
