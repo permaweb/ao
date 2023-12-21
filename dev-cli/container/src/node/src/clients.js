e427c2d5676430f26a77534a37ea4ad57d954aa0
Tyler Hall <tyler@hyper.io>
2023-12-21 15:41:57 +0000

    feat(dev-cli): implement bundler and publish apis for up.arweave.net. Use latest SDK #243 #223

--- dev-cli/container/src/node/src/clients.js
+++ dev-cli/container/src/node/src/clients.js
@@ -1,6 +1,15 @@
+import { readFileSync } from 'node:fs'
+import { randomBytes } from 'node:crypto'
+
+import urlJoin from 'url-join'
+import mime from 'mime-types'
+import Arweave from 'arweave'
 import Irys from '@irys/sdk'
+import WarpArBundles from 'warp-arbundles'
 
 import { SUPPORTED_BUNDLERS } from './main.js'
+
+const { createData, ArweaveSigner } = WarpArBundles
 
 /**
  * Client map that contains per client implementations
@@ -11,14 +20,49 @@ import { SUPPORTED_BUNDLERS } from './main.js'
 export const UPLOADERS = {
   [SUPPORTED_BUNDLERS.IRYS]: ({ path, wallet, to: irysNode, ...rest }) => {
     const irys = new Irys({ url: irysNode, token: 'arweave', key: wallet })
+    /**
+     * Response implements { id }
+     */
     return irys.uploadFile(path, rest)
   },
-  [SUPPORTED_BUNDLERS.UP]: ({ path, wallet, to: irysNode, ...rest }) => {
+  /**
+   * TODO: impl could be better
+   */
+  [SUPPORTED_BUNDLERS.UP]: async ({ path, wallet, to, ...rest }) => {
+    const signer = new ArweaveSigner(wallet)
+
+    rest.tags = rest.tags || []
+
     /**
-     * TODO: do not use the Irys SDK
+     * Add the Content-Type tag, if it's not already provided,
+     * according to the mime-type obtained from the file path
      */
-    const irys = new Irys({ url: irysNode, token: 'arweave', key: wallet })
-    return irys.uploadFile(path, rest)
+    if (!rest.tags.find((t) => t.name.toLowerCase() === 'content-type')) {
+      const mimeType = mime.contentType(mime.lookup(path) || 'application/octet-stream')
+      rest.tags.push({ name: 'Content-Type', value: mimeType })
+    }
+
+    /**
+     * Create and sign the data item, setting the data to the
+     * buffer containing the file contents
+     */
+    const dataItem = createData(readFileSync(path), signer, {
+      anchor: randomBytes(32).toString('base64').slice(0, 32),
+      ...rest
+    })
+    await dataItem.sign(signer)
+
+    /**
+     * Response implements { id }
+     */
+    return fetch(urlJoin(to, '/tx/arweave'), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        Accept: 'application/json'
+      },
+      body: dataItem.getRaw()
+    }).then((res) => res.json())
   }
 }
 
@@ -27,8 +71,13 @@ export const BALANCERS = {
     const irys = new Irys({ url: irysNode, token: 'arweave', key: wallet })
     return irys.getLoadedBalance().then(balance => ({ balance }))
   },
-  [SUPPORTED_BUNDLERS.UP]: () => {
-    return { balance: 'This Bundler is 100% subsidized. No balance is required' }
+  [SUPPORTED_BUNDLERS.UP]: async ({ wallet, to }) => {
+    const arweave = Arweave.init()
+    const address = await arweave.wallets.jwkToAddress(wallet)
+
+    return fetch(urlJoin(to, '/account/balance', address))
+      .then((res) => res.json())
+      .then((balance) => ({ balance: balance.toLocaleString() }))
   }
 }
 
@@ -38,6 +87,6 @@ export const FUNDERS = {
     return irys.fund(amount).then(res => ({ id: res.id }))
   },
   [SUPPORTED_BUNDLERS.UP]: () => {
-    return { id: 'This Bundler is 100% subsidized. No funding is required' }
+    return { id: 'up.arweave.net is 100% subsidized and requires no funding' }
   }
 }
