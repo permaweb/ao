d336088396ce935e1a27dc9305800b0eeda0be90
Tyler Hall <tyler@hyper.io>
2024-01-02 19:45:29 +0000

    chore(lua-examples): bump deps

--- lua-examples/hello-world/readResult.js
+++ lua-examples/hello-world/readResult.js
@@ -1,11 +1,13 @@
 import { connect } from '@permaweb/ao-sdk'
 
 const MESSAGE_ID = process.env.MESSAGE_ID
+const PROCESS_ID = process.env.PROCESS_ID
 
 if (!MESSAGE_ID) throw new Error('MESSAGE_ID env var is required, so as to know which process is receiving the message')
+if (!PROCESS_ID) throw new Error('PROCESS_ID env var is required, so as to know which process is receiving the message')
 
 const { result } = connect()
 
-await result({ message: MESSAGE_ID })
+await result({ message: MESSAGE_ID, process: PROCESS_ID })
   .then(console.log)
   .catch(console.error)
