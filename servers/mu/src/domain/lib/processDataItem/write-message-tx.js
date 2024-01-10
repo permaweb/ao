44cbb89c21264752c9d3e7ee43ca86ea262786ab
Vince Juliano <v.juliano6363@gmail.com>
2024-01-10 12:06:35 -0500

    feat(mu): modify oracle support to be based on target

--- servers/mu/src/domain/lib/processDataItem/write-message-tx.js
+++ servers/mu/src/domain/lib/processDataItem/write-message-tx.js
@@ -1,4 +1,4 @@
-import { of, fromPromise } from 'hyper-async'
+import { of, fromPromise, Rejected } from 'hyper-async'
 import { __, assoc, tap } from 'ramda'
 import z from 'zod'
 
@@ -9,30 +9,60 @@ const ctxSchema = z.object({
   }).passthrough()
 }).passthrough()
 
+const ctxSchemaArweave = z.object({
+  arweaveTx: z.object({
+    id: z.string(),
+    timestamp: z.number()
+  }).passthrough()
+}).passthrough()
+
 export function writeMessageTxWith (env) {
-  let { logger, writeDataItem, locateProcess } = env
+  let { logger, writeDataItem, locateProcess, writeDataItemArweave } = env
 
   writeDataItem = fromPromise(writeDataItem)
   locateProcess = fromPromise(locateProcess)
+  writeDataItemArweave = fromPromise(writeDataItemArweave)
 
   return (ctx) => {
     return of()
-      .map(tap(() => ctx.tracer.trace('Sending message to SU')))
+      .map(tap(() => ctx.tracer.trace('Sending message to SU or Arweave')))
       .chain(() =>
         locateProcess(ctx.tx.processId)
-          .chain(({ url }) => writeDataItem({ suUrl: url, data: ctx.tx.data.toString('base64') }))
-      )
-      .map(assoc('schedulerTx', __, ctx))
-      /**
-       * Make sure to add the message's id
-       * to the trace of the parent message
-       */
-      .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
-      .map(ctxSchema.parse)
-      .map(logger.tap('Added "schedulerTx" to ctx'))
-      .bimap(
-        tap(() => ctx.tracer.trace('Failed to send message to SU')),
-        tap(() => ctx.tracer.trace('Sent message to SU'))
+          .bichain(
+            (error) => {
+              if (error.name === 'TransactionNotFound' || error.name === 'SchedulerTagNotFound') {
+                return writeDataItemArweave(ctx.tx.data)
+                  .map(assoc('arweaveTx', __, ctx))
+                  /**
+                   * Make sure to add the message's id
+                   * to the trace of the parent message
+                   */
+                  .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
+                  .map(ctxSchemaArweave.parse)
+                  .map(logger.tap('Added "arweaveTx" to ctx'))
+                  .bimap(
+                    tap(() => ctx.tracer.trace('Failed to send message to Arweave')),
+                    tap(() => ctx.tracer.trace('Sent message to Arweave'))
+                  )
+              }
+              return Rejected(error)
+            },
+            ({ url }) => {
+              return writeDataItem({ suUrl: url, data: ctx.tx.data.toString('base64') })
+                .map(assoc('schedulerTx', __, ctx))
+                /**
+                 * Make sure to add the message's id
+                 * to the trace of the parent message
+                 */
+                .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
+                .map(ctxSchema.parse)
+                .map(logger.tap('Added "schedulerTx" to ctx'))
+                .bimap(
+                  tap(() => ctx.tracer.trace('Failed to send message to SU')),
+                  tap(() => ctx.tracer.trace('Sent message to SU'))
+                )
+            }
+          )
       )
   }
 }
