bb41e9180dfe4d8ee27fd3211501c2c479fdda58
Tom Wilson <tom@hyper.io>
2024-01-14 23:55:52 +0000

    feat: add unmonitor feature #310

---
+++ connect/src/lib/unmonitor/index.js
@@ -0,0 +1,36 @@
+import { identity } from 'ramda'
+import { of } from 'hyper-async'
+
+// eslint-disable-next-line no-unused-vars
+import { Types } from '../../dal.js'
+import { errFrom } from '../utils.js'
+import { verifyProcessWith } from '../verify-process.js'
+import { uploadUnmonitorWith } from './upload-unmonitor.js'
+
+/**
+ * @typedef Env1
+ *
+ * TODO: maybe allow passing tags and anchor eventually?
+ * @typedef SendMonitorArgs
+ * @property {string} process
+ * @property {string} [data]
+ * @property {Types['signer']} signer
+ *
+ * @callback SendMonitor
+ * @param {SendMonitorArgs} args
+ * @returns {Promise<string>} the id of the data item that represents this message
+ *
+ * @param {Env1} - the environment
+ * @returns {SendMonitor}
+ */
+export function unmonitorWith (env) {
+  const verifyProcess = verifyProcessWith(env)
+  const uploadUnmonitor = uploadUnmonitorWith(env)
+
+  return ({ process, signer }) => of({ id: process, signer })
+    .chain(verifyProcess)
+    .chain(uploadUnmonitor)
+    .map((ctx) => ctx.monitorId)
+    .bimap(errFrom, identity)
+    .toPromise()
+}
---
+++ connect/src/lib/unmonitor/upload-unmonitor.js
@@ -0,0 +1,49 @@
+import { fromPromise, of } from 'hyper-async'
+import { assoc } from 'ramda'
+
+import { deployMonitorSchema, signerSchema } from '../../dal.js'
+
+/**
+ * @typedef Tag3
+ * @property {string} name
+ * @property {any} value
+ *
+ * @typedef Context3
+ * @property {string} id - the transaction id to be verified
+ * @property {any} input
+ * @property {any} wallet
+ * @property {Tag3[]} tags
+ *
+ * @typedef Env6
+ * @property {any} mu
+ */
+
+/**
+ * @callback BuildTx
+ * @param {Context3} ctx
+ * @returns {Async<Context3>}
+ *
+ * @param {Env6} env
+ * @returns {BuildTx}
+ */
+export function uploadUnmonitorWith (env) {
+  const deployUnmonitor = deployMonitorSchema.implement(env.deployUnmonitor)
+
+  return (ctx) => {
+    return of(ctx)
+      .chain(fromPromise(({ id, signer }) =>
+        deployUnmonitor({
+          processId: id,
+          signer: signerSchema.implement(signer),
+          /**
+           * No tags or data can be provided right now,
+           *
+           * so just randomize data and set tags to an empty array
+           */
+          data: Math.random().toString().slice(-4),
+          tags: []
+        })
+      ))
+      .map(res => assoc('monitorId', res.messageId, ctx))
+  }
+}
