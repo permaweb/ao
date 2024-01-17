57226a5d07ceadffbde452bf607f87defc592a91
Hlöðver Sigurðsson <hlolli@gmail.com>
2024-01-17 13:07:11 +0100

    infra: code-pattern for storing secrets securely

---
+++ servers/mu/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# make a wallet.json file from secrets-manager
+aws secretsmanager get-secret-value --secret-id ao-wallet --query SecretString --output text > wallet.json
+
+# make a .env file
+echo "PATH_TO_WALLET=wallet.json" >> .env
+
+# start the server
+node src/app.js
---
+++ terraform/src/secrets.tf
@@ -0,0 +1,3 @@
+data "aws_secretsmanager_secret" "ao-wallet" {
+  arn = "arn:aws:secretsmanager:us-west-1:429478883069:secret:ao-wallet-0gBKzZ"
+}
