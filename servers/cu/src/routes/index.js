698c9040201afa896d1cc3e69da8a3969dc0f272
Tyler Hall <tyler@hyper.io>
2024-01-22 20:30:03 +0000

    feat(cu): add /results endpoint #344

--- servers/cu/src/routes/index.js
+++ servers/cu/src/routes/index.js
@@ -2,6 +2,7 @@ import { pipe } from 'ramda'
 
 import { withStateRoutes } from './state.js'
 import { withResultRoutes } from './result.js'
+import { withResultsRoutes } from './results.js'
 import { withCronRoutes } from './cron.js'
 import { withHealthcheckRoutes } from './healthcheck.js'
 
@@ -9,5 +10,6 @@ export const withRoutes = pipe(
   withHealthcheckRoutes,
   withStateRoutes,
   withResultRoutes,
+  withResultsRoutes,
   withCronRoutes
 )
