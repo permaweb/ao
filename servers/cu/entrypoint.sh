1136a5220a4bfd8f0dcf53b168df661a6d0f1a23
Hlöðver Sigurðsson <hlolli@gmail.com>
2024-01-23 22:46:51 +0100

    fix(ci): packer setup script

--- servers/cu/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-WALLET=$(aws secretsmanager get-secret-value --secret-id ao-wallet --query SecretString --output text)
-
-# make a .env file
-echo "WALLET=$WALLET" >> .env
-
-# start the server
-node src/app.js
