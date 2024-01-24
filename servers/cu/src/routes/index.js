8ca5a7f29e3147948c87fd3ef021848f19a24dd2
Tyler Hall <tyler@hyper.io>
2024-01-24 17:11:53 +0000

    feat(cu,ur): dry run routes #359

--- servers/cu/src/routes/index.js
+++ servers/cu/src/routes/index.js
@@ -2,6 +2,7 @@ import { pipe } from 'ramda'
 
 import { withStateRoutes } from './state.js'
 import { withResultRoutes } from './result.js'
+import { withDryRunRoutes } from './dryRun.js'
 import { withResultsRoutes } from './results.js'
 import { withCronRoutes } from './cron.js'
 import { withHealthcheckRoutes } from './healthcheck.js'
@@ -10,6 +11,7 @@ export const withRoutes = pipe(
   withHealthcheckRoutes,
   withStateRoutes,
   withResultRoutes,
+  withDryRunRoutes,
   withResultsRoutes,
   withCronRoutes
 )
