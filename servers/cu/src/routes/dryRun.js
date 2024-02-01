ee2d4188f0c82fcafea80683f66bd0a79a129fe8
Tyler Hall <tyler@hyper.io>
2024-02-01 15:08:40 +0000

    feat(cu): allow more fields on dryRun messages #407

--- servers/cu/src/routes/dryRun.js
+++ servers/cu/src/routes/dryRun.js
@@ -6,16 +6,19 @@ import { withMiddleware } from './middleware/index.js'
 const inputSchema = z.object({
   processId: z.string().min(1, 'a process-id query parameter is required'),
   messageTxId: z.string().min(1, 'to must be a transaction id').optional(),
-  /**
-   * TODO: I think dryRun messages would only need Data and Tags.
-   * DO we think we need to allow anything else?
-   */
   dryRun: z.object({
+    Id: z.string().nullish(),
+    Signature: z.string().nullish(),
+    Owner: z.string().nullish(),
+    Target: z.string().nullish(),
     Data: z.any().nullish(),
     Tags: z.array(z.object({
       name: z.string(),
       value: z.string()
-    })).nullish()
+    })).nullish(),
+    Anchor: z.string().nullish(),
+    Timestamp: z.coerce.number().nullish(),
+    'Block-Height': z.coerce.number().nullish()
   }).passthrough()
 })
 
