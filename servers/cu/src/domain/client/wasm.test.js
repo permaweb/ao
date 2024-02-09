0fbd4f4e113a9531452a459a0dbd4e69e971590a
Tyler Hall <tyler@hyper.io>
2024-02-09 23:13:05 +0000

    feat(cu): hashWasmMemory api #404

--- servers/cu/src/domain/client/wasm.test.js
+++ servers/cu/src/domain/client/wasm.test.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-throw-literal */
 import { describe, test } from 'node:test'
 import assert from 'node:assert'
+import { createReadStream } from 'node:fs'
 
 import { doesExceedMaximumHeapSizeSchema } from '../dal.js'
-import { doesExceedMaximumHeapSizeWith } from './wasm.js'
+import { doesExceedMaximumHeapSizeWith, hashWasmMemory } from './wasm.js'
+import { createGzip } from 'node:zlib'
 
 describe('wasm', () => {
   describe('doesExceedMaximumHeapSize', () => {
@@ -24,6 +26,30 @@ describe('wasm', () => {
         await doesExceedMaximumHeapSize({ heap: Buffer.from(tenChars + '1', 'utf-8') }),
         true
       )
+    })
+  })
+
+  describe('hashWasmMemory', () => {
+    test('should hash the array buffer', async () => {
+      const s = createReadStream('./test/processes/happy/process.wasm')
+
+      const sha = await hashWasmMemory(s)
+
+      assert.ok(typeof sha === 'string')
+      assert.equal(sha.length, 64)
+    })
+
+    test('should decode the array buffer before hashing', async () => {
+      const raw = createReadStream('./test/processes/happy/process.wasm')
+      const rawSha = await hashWasmMemory(raw)
+
+      const encoded = createReadStream('./test/processes/happy/process.wasm')
+        .pipe(createGzip())
+
+      const encodedSha = await hashWasmMemory(encoded, 'gzip')
+
+      assert.equal(encodedSha.length, 64)
+      assert.equal(rawSha, encodedSha)
     })
   })
 })
