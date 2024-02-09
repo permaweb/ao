e2b17d05e940d86b82404040e6df70e884d99909
Tyler Hall <tyler@hyper.io>
2024-02-09 23:13:05 +0000

    feat(cu): hashWasmMemory api #404

--- servers/cu/src/domain/client/wasm.js
+++ servers/cu/src/domain/client/wasm.js
@@ -1,9 +1,10 @@
 import { tmpdir } from 'node:os'
 import { createWriteStream, readFile } from 'node:fs'
 import { promisify } from 'node:util'
-import { pipeline } from 'node:stream'
+import { PassThrough, pipeline } from 'node:stream'
 import { join } from 'node:path'
-import { createGzip, gunzip } from 'node:zlib'
+import { createGzip, createGunzip, gunzip } from 'node:zlib'
+import { createHash } from 'node:crypto'
 
 const readFileP = promisify(readFile)
 const gunzipP = promisify(gunzip)
@@ -20,6 +21,34 @@ export async function writeWasmFile (moduleId, wasmStream) {
     createGzip(),
     createWriteStream(join(tmpdir(), `${moduleId}.wasm.gz`))
   )
+}
+
+/**
+ * The memory may be encoded, so in order to compute the correct hash
+ * of the actual memory, we may need to decode it
+ *
+ * We use a stream, so that we can incrementally compute hash in a non-blocking way
+ */
+export async function hashWasmMemory (memoryStream, encoding) {
+  /**
+   * TODO: add more encoding options
+   */
+  if (encoding && encoding !== 'gzip') {
+    throw new Error('Only GZIP encoding of Memory is supported for Process Checkpoints')
+  }
+
+  return Promise.resolve(memoryStream)
+    .then((memoryStream) => {
+      const hash = createHash('sha256')
+      return pipelineP(
+        memoryStream,
+        encoding === 'gzip'
+          ? createGunzip()
+          : new PassThrough(),
+        hash
+      )
+        .then(() => hash.digest('hex'))
+    })
 }
 
 export function doesExceedMaximumHeapSizeWith ({ PROCESS_WASM_HEAP_MAX_SIZE }) {
