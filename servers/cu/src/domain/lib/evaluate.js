10a63c6f06f3388d5f5678ab1524f762e2f67eeb
Tyler Hall <tyler@hyper.io>
2024-02-21 22:35:21 +0000

    refactor(cu): move error handling and fold logic into worker and out of main thread

--- servers/cu/src/domain/lib/evaluate.js
+++ servers/cu/src/domain/lib/evaluate.js
@@ -1,10 +1,7 @@
 import { compose as composeStreams } from 'node:stream'
 import { finished } from 'node:stream/promises'
 
-import {
-  T, always, applySpec, assocPath, cond, defaultTo, identity,
-  ifElse, is, mergeRight, pathOr, pipe, propOr
-} from 'ramda'
+import { always, applySpec, identity, mergeLeft, mergeRight, pathOr } from 'ramda'
 import { Rejected, Resolved, fromPromise, of } from 'hyper-async'
 import { z } from 'zod'
 
@@ -104,89 +101,10 @@ export function evaluateWith (env) {
 
   const saveLatestProcessMemory = env.saveLatestProcessMemory
 
-  /**
-   * Given the previous interaction output,
-   * return a function that will merge the next interaction output
-   * with the previous.
-   */
-  const mergeOutput = (prev, { noSave, message, cron, ordinate }) => pipe(
-    defaultTo({}),
-    applySpec({
-      /**
-       * We default to 'new' state, from applying an interaction,
-       * to the previous state, but it will be overwritten by the outputs state
-       *
-       * This ensures the new interaction in the chain has state to
-       * operate on, even if the previous interaction only produced
-       * messages and no state change.
-       *
-       * If the output contains an error, ignore its state,
-       * and use the previous message's state
-       */
-      Memory: ifElse(
-        pathOr(undefined, ['Error']),
-        always(prev.Memory),
-        propOr(prev.Memory, 'Memory')
-      ),
-      Error: pathOr(undefined, ['Error']),
-      Messages: pathOr([], ['Messages']),
-      Spawns: pathOr([], ['Spawns']),
-      Output: pipe(
-        pathOr('', ['Output']),
-        /**
-         * Always make sure the output
-         * is a string or object
-         */
-        cond([
-          [is(String), identity],
-          [is(Object), identity],
-          [is(Number), String],
-          [T, identity]
-        ])
-      ),
-      GasUsed: pathOr(undefined, ['GasUsed']),
-      /**
-       * These values are folded,
-       * so that we can potentially update the process memory cache
-       * at the end of evaluation
-       */
-      noSave: always(noSave),
-      message: always(message),
-      cron: always(cron),
-      ordinate: always(ordinate)
-    })
-  )
-
   return (ctx) =>
     of(ctx)
       .chain(loadEvaluator)
       .chain(fromPromise(async (ctx) => {
-        /**
-         * There seems to be duplicate Cron Message evaluations occurring and it's been difficult
-         * to pin down why. My hunch is that the very first message can be a duplicate of the 'from', if 'from'
-         * is itself a Cron Message.
-         *
-         * So to get around this, we maintain a set of strings that unique identify
-         * Cron messages (timestamp+cron interval). We will add each cron message identifier to this Set.
-         * If an iteration comes across an identifier already present in this list, then we consider it
-         * a duplicate and remove it from the eval stream.
-         *
-         * This should prevent duplicate Cron Messages from being duplicate evaluated, thus not "tainting"
-         * Memory that is folded as part of the eval stream
-         */
-        const evaledCrons = new Set()
-        /**
-          * If the starting point ('from') is itself a Cron Message,
-          * then that will be our first identifier added to the set
-          */
-        if (ctx.fromCron) evaledCrons.add(toEvaledCron({ timestamp: ctx.from, cron: ctx.fromCron }))
-
-        /**
-         * Keep track of any deepHashes in the eval stream
-         * as a quick way to eliminate dupes in the same eval stream
-         */
-        const deepHashes = new Set()
-
         let prev = applySpec({
           /**
            * Ensure all result fields are initialized
@@ -204,6 +122,32 @@ export function evaluateWith (env) {
         await finished(composeStreams(
           ctx.messages,
           async function (messages) {
+            /**
+             * There seems to be duplicate Cron Message evaluations occurring and it's been difficult
+             * to pin down why. My hunch is that the very first message can be a duplicate of the 'from', if 'from'
+             * is itself a Cron Message.
+             *
+             * So to get around this, we maintain a set of strings that unique identify
+             * Cron messages (timestamp+cron interval). We will add each cron message identifier to this Set.
+             * If an iteration comes across an identifier already present in this list, then we consider it
+             * a duplicate and remove it from the eval stream.
+             *
+             * This should prevent duplicate Cron Messages from being duplicate evaluated, thus not "tainting"
+             * Memory that is folded as part of the eval stream
+             */
+            const evaledCrons = new Set()
+            /**
+             * If the starting point ('from') is itself a Cron Message,
+             * then that will be our first identifier added to the set
+             */
+            if (ctx.fromCron) evaledCrons.add(toEvaledCron({ timestamp: ctx.from, cron: ctx.fromCron }))
+
+            /**
+             * Keep track of any deepHashes in the eval stream
+             * as a quick way to eliminate dupes in the same eval stream
+             */
+            const deepHashes = new Set()
+
             /**
              * Iterate over the async iterable of messages,
              * and evaluate each one
@@ -248,33 +192,26 @@ export function evaluateWith (env) {
                      */
                     .then((Memory) => ctx.evaluator({ name, processId: ctx.id, Memory, message, AoGlobal }))
                     /**
-                     * Map thrown error to a result.error
+                     * These values are folded,
+                     * so that we can potentially update the process memory cache
+                     * at the end of evaluation
                      */
-                    .catch((err) => Promise.resolve(assocPath(['Error'], err, {})))
-                    /**
-                     * The the previous interaction output, and merge it
-                     * with the output of the current interaction
-                     */
-                    .then(mergeOutput(prev, { noSave, message, cron, ordinate }))
+                    .then(mergeLeft({ noSave, message, cron, ordinate }))
                     .then(async (output) => {
                       if (cron) ctx.stats.messages.cron++
                       else ctx.stats.messages.scheduled++
 
                       return Promise.resolve(output)
                         .then((output) => {
-                          return output.Error
-                            ? Promise.reject(output)
-                            : Promise.resolve(output)
-                        })
-                        /**
-                         * Create a new evaluation to be cached in the local db
-                         */
-                        .then((output) => {
+                          if (output.Error) return Promise.reject(output)
                           /**
                            * Noop saving the evaluation is noSave flag is set
                            */
-                          if (noSave) return output
+                          if (noSave) return
 
+                          /**
+                           * Create a new evaluation to be cached in the local db
+                           */
                           return saveEvaluation({
                             name,
                             deepHash,
@@ -289,9 +226,9 @@ export function evaluateWith (env) {
                             evaluatedAt: new Date(),
                             output
                           })
-                            .map(() => output)
                             .toPromise()
                         })
+                        .then(() => output)
                         .catch((err) => {
                           logger(
                             'Error occurred when applying message "%s" to process "%s": "%s',
