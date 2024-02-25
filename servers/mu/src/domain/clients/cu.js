e68c4fa4d9cb6ee3292ae054b96e037e68a17ddc
Tyler Hall <tyler@hyper.io>
2024-02-25 16:49:24 +0000

    chore(mu): add passing no-busy query param to CU /result #494

--- servers/mu/src/domain/clients/cu.js
+++ servers/mu/src/domain/clients/cu.js
@@ -1,12 +1,12 @@
 function resultWith ({ fetch, CU_URL, logger }) {
   return async (txId, processId) => {
-    logger(`${CU_URL}/result/${txId}?process-id=${processId}`)
+    logger(`${CU_URL}/result/${txId}?process-id=${processId}?no-busy=1`)
 
     const requestOptions = {
       timeout: 0
     }
 
-    return fetch(`${CU_URL}/result/${txId}?process-id=${processId}`, requestOptions)
+    return fetch(`${CU_URL}/result/${txId}?process-id=${processId}?no-busy=1`, requestOptions)
       .then(res => res.json())
       .then(res => res || {
         Messages: [],
