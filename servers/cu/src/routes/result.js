bb2d2e181aeb34a5e82e42d8a6dd9f50dea660b3
Tyler Hall <tyler@hyper.io>
2024-02-25 16:45:08 +0000

    feat(cu): add busy threshold to /result, and allow disable with no-busy query param #494

--- servers/cu/src/routes/result.js
+++ servers/cu/src/routes/result.js
@@ -1,6 +1,7 @@
 import { compose } from 'ramda'
 import { z } from 'zod'
 
+import { busyIn } from '../domain/utils.js'
 import { withMiddleware } from './middleware/index.js'
 import { withInMemoryCache } from './middleware/withInMemoryCache.js'
 
@@ -19,17 +20,39 @@ export const withResultRoutes = app => {
           const { params: { messageTxId } } = req
           return messageTxId
         },
-        loader: async ({ req }) => {
+        loader: async ({ req, res }) => {
           const {
             params: { messageTxId },
-            query: { 'process-id': processId },
-            domain: { apis: { readResult } }
+            /**
+             * Client may set the 'no-busy' query parameter to any value
+             * to disable the busy response.
+             *
+             * Useful for MUs that would rather wait on the evaluation response
+             */
+            query: { 'process-id': processId, 'no-busy': noBusy },
+            domain: { BUSY_THRESHOLD, apis: { readResult } }
           } = req
+
+          console.log(noBusy)
 
           const input = inputSchema.parse({ messageTxId, processId })
 
-          return readResult(input)
-            .toPromise()
+          return busyIn(
+            noBusy ? 0 : BUSY_THRESHOLD,
+            readResult(input)
+              .map((output) => [output])
+              .toPromise(),
+            () => {
+              res.status(202)
+              return [
+                { message: `Evaluation of process "${input.processId}" to "${input.messageTxId || 'latest'}" is in progress.` },
+                /**
+                 * Don't store the busy message in the In-Memory cache
+                 */
+                true // noCache
+              ]
+            }
+          )
         }
       })
     )()
