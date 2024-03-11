6cdeb2fd343b3aeb2c9f2d3f5390ec736bd2f543
Vince Juliano <v.juliano6363@gmail.com>
2024-03-11 14:42:35 -0400

    fix(mu): block multiple monitors #528

--- servers/mu/src/domain/clients/os.js
+++ servers/mu/src/domain/clients/os.js
@@ -2,45 +2,49 @@ import { spawn } from 'child_process'
 import fs from 'fs'
 import os from 'os'
 
-let procs = {}
+const procs = {}
 
-function startMonitoredProcessWith({ logger }) {
-    return async ({ id }) => {
-        console.log('OS run ID:', id)
-        const logFilePath = `${os.tmpdir()}/${id}.log`
-        const out = fs.openSync(logFilePath, 'a')
-        const err = fs.openSync(logFilePath, 'a')
+function startMonitoredProcessWith ({ logger }) {
+  return async ({ id }) => {
+    console.log('OS run ID:', id)
+    const logFilePath = `${os.tmpdir()}/${id}.log`
+    const out = fs.openSync(logFilePath, 'a')
+    const err = fs.openSync(logFilePath, 'a')
 
-        const child = spawn('node', ['cranker/src/index.js', id], {
-            stdio: ['ignore', out, err], 
-        });
-
-        if (child && child.pid) {
-            console.log(`Command executed with PID: ${child.pid}`)
-            procs[id] = child
-            return child.pid
-        } else {
-            console.log('Failed to execute command');
-            throw new Error('Failed to execute command');
-        }
+    if (procs[id]) {
+      throw new Error('Process already being monitored')
     }
+
+    const child = spawn('node', ['cranker/src/index.js', id], {
+      stdio: ['ignore', out, err]
+    })
+
+    if (child && child.pid) {
+      console.log(`Command executed with PID: ${child.pid}`)
+      procs[id] = child
+      return child.pid
+    } else {
+      console.log('Failed to execute command')
+      throw new Error('Failed to execute command')
+    }
+  }
 }
 
-function killMonitoredProcessWith({ logger }) {
-    return async ({ id }) => {
-        const proc = procs[id]
-        if (proc) {
-            proc.kill()
-            console.log(`Process with PID: ${proc.pid} has been killed.`)
-            delete procs[id]
-        } else {
-            console.log(`No process found for ID: ${id}`)
-            throw new Error(`No process found for ID: ${id}`)
-        }
+function killMonitoredProcessWith ({ logger }) {
+  return async ({ id }) => {
+    const proc = procs[id]
+    if (proc) {
+      proc.kill()
+      console.log(`Process with PID: ${proc.pid} has been killed.`)
+      delete procs[id]
+    } else {
+      console.log(`No process found for ID: ${id}`)
+      throw new Error(`No process found for ID: ${id}`)
     }
+  }
 }
 
 export default {
-    startMonitoredProcessWith,
-    killMonitoredProcessWith
-};
+  startMonitoredProcessWith,
+  killMonitoredProcessWith
+}
