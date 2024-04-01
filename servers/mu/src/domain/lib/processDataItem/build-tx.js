8d25544e8c8446e4ec81d973e59b9c305d0ff445
Vince Juliano <v.juliano6363@gmail.com>
2024-04-01 18:16:06 -0400

    fix(mu): initial code changes for #559

--- servers/mu/src/domain/lib/processDataItem/build-tx.js
+++ servers/mu/src/domain/lib/processDataItem/build-tx.js
@@ -1,27 +1,45 @@
-import { of, fromPromise } from 'hyper-async'
+import { fromPromise, Resolved, Rejected } from 'hyper-async'
 import z from 'zod'
-import { assoc, __, tap } from 'ramda'
+import { tap } from 'ramda'
 
 const ctxSchema = z.object({
   tx: z.object({
     id: z.string(),
     data: z.any(),
     processId: z.string()
-  })
+  }),
+  schedLocation: z.any().nullable()
 }).passthrough()
 
 export function buildTxWith (env) {
   let { buildAndSign, logger, locateProcess, fetchSchedulerProcess } = env
-
   locateProcess = fromPromise(locateProcess)
   fetchSchedulerProcess = fromPromise(fetchSchedulerProcess)
   buildAndSign = fromPromise(buildAndSign)
 
   return (ctx) => {
-    return of(ctx)
-      .map(tap(() => ctx.tracer.trace('Building and signing message from outbox')))
-      .chain(() => locateProcess(ctx.cachedMsg.processId))
-      .chain((res) => fetchSchedulerProcess(ctx.cachedMsg.processId, res.url))
+    return locateProcess(ctx.cachedMsg.processId)
+      .bichain(
+        (error) => {
+          /*
+              If it is one of these errors This means  that it is supposed
+              to go directly to Arweave. If it is some other error we need
+              to reject.
+          */
+          if (error.name !== 'TransactionNotFound' && error.name !== 'SchedulerTagNotFound') {
+            return Rejected(error)
+          }
+          return Resolved()
+        },
+        (scheduler) => {
+          /*
+            We have a scheduler so return that for
+            later use in the pipeline
+          */
+          return fetchSchedulerProcess(ctx.cachedMsg.processId, scheduler.url)
+            .map((schedulerResult) => ({ schedLocation: scheduler, schedData: schedulerResult }))
+        }
+      )
       .map((res) => {
         const tagsIn = [
           ...ctx.cachedMsg.msg.Tags?.filter((tag) => {
@@ -31,24 +49,28 @@ export function buildTxWith (env) {
           { name: 'Type', value: 'Message' },
           { name: 'Variant', value: 'ao.TN.1' },
           { name: 'From-Process', value: ctx.cachedMsg.processId },
-          { name: 'From-Module', value: res.tags.find((t) => t.name === 'Module')?.value ?? '' }
+          { name: 'From-Module', value: res.schedData.tags.find((t) => t.name === 'Module')?.value ?? '' }
         ]
         if (ctx.cachedMsg.initialTxId) {
           tagsIn.push({ name: 'Pushed-For', value: ctx.cachedMsg.initialTxId })
         }
-        return tagsIn
+        return { tags: tagsIn, scheduler: res.schedLocation }
       })
       .chain(
-        (tags) => buildAndSign({
+        ({ tags, schedLocation }) => buildAndSign({
           processId: ctx.cachedMsg.msg.Target,
           tags,
           anchor: ctx.cachedMsg.msg.Anchor,
           data: ctx.cachedMsg.msg.Data
         })
+          .map((tx) => { return { tx, schedLocation } })
       )
-      .map(assoc('tx', __, ctx))
+      .map((res) => {
+        // add tx and schedLocation to the result
+        return { ...ctx, ...res }
+      })
       .map(ctxSchema.parse)
-      .map(logger.tap('Added tx to ctx'))
+      .map(logger.tap('Added tx and schedLocation to ctx'))
       .bimap(
         tap(() => ctx.tracer.trace('Failed to build and sign message from outbox')),
         tap(() => ctx.tracer.trace('Built and signed message from outbox'))
--- servers/mu/src/domain/lib/processDataItem/write-message-tx.js
+++ servers/mu/src/domain/lib/processDataItem/write-message-tx.js
@@ -1,4 +1,4 @@
-import { of, fromPromise, Rejected } from 'hyper-async'
+import { fromPromise } from 'hyper-async'
 import { __, assoc, tap } from 'ramda'
 import z from 'zod'
 
@@ -17,52 +17,44 @@ const ctxSchemaArweave = z.object({
 }).passthrough()
 
 export function writeMessageTxWith (env) {
-  let { logger, writeDataItem, locateProcess, writeDataItemArweave } = env
+  let { logger, writeDataItem, writeDataItemArweave } = env
 
   writeDataItem = fromPromise(writeDataItem)
-  locateProcess = fromPromise(locateProcess)
   writeDataItemArweave = fromPromise(writeDataItemArweave)
 
   return (ctx) => {
-    return of()
-      .map(tap(() => ctx.tracer.trace('Sending message to SU or Arweave')))
-      .chain(() =>
-        locateProcess(ctx.tx.processId)
-          .bichain(
-            (error) => {
-              if (error.name === 'TransactionNotFound' || error.name === 'SchedulerTagNotFound') {
-                return writeDataItemArweave(ctx.tx.data)
-                  .map(assoc('arweaveTx', __, ctx))
-                  /**
-                   * Make sure to add the message's id
-                   * to the trace of the parent message
-                   */
-                  .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
-                  .map(ctxSchemaArweave.parse)
-                  .map(logger.tap('Added "arweaveTx" to ctx'))
-                  .bimap(
-                    tap(() => ctx.tracer.trace('Failed to send message to Arweave')),
-                    tap(() => ctx.tracer.trace('Sent message to Arweave'))
-                  )
-              }
-              return Rejected(error)
-            },
-            ({ url }) => {
-              return writeDataItem({ suUrl: url, data: ctx.tx.data.toString('base64') })
-                .map(assoc('schedulerTx', __, ctx))
-                /**
-                 * Make sure to add the message's id
-                 * to the trace of the parent message
-                 */
-                .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
-                .map(ctxSchema.parse)
-                .map(logger.tap('Added "schedulerTx" to ctx'))
-                .bimap(
-                  tap(() => ctx.tracer.trace('Failed to send message to SU')),
-                  tap(() => ctx.tracer.trace('Sent message to SU'))
-                )
-            }
-          )
-      )
+    /*
+      If we have schedLocation write to the scheduler.
+      If not write to Arweave
+    */
+    if (ctx.schedLocation) {
+      return writeDataItem({ suUrl: ctx.schedLocation.url, data: ctx.tx.data.toString('base64') })
+        .map(assoc('schedulerTx', __, ctx))
+        /**
+         * Make sure to add the message's id
+         * to the trace of the parent message
+         */
+        .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
+        .map(ctxSchema.parse)
+        .map(logger.tap('Added "schedulerTx" to ctx'))
+        .bimap(
+          tap(() => ctx.tracer.trace('Failed to send message to SU')),
+          tap(() => ctx.tracer.trace('Sent message to SU'))
+        )
+    } else {
+      return writeDataItemArweave(ctx.tx.data)
+        .map(assoc('arweaveTx', __, ctx))
+        /**
+         * Make sure to add the message's id
+         * to the trace of the parent message
+         */
+        .map(tap(ctx => ctx.tracer.child(ctx.tx.id)))
+        .map(ctxSchemaArweave.parse)
+        .map(logger.tap('Added "arweaveTx" to ctx'))
+        .bimap(
+          tap(() => ctx.tracer.trace('Failed to send message to Arweave')),
+          tap(() => ctx.tracer.trace('Sent message to Arweave'))
+        )
+    }
   }
 }
