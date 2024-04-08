a481fbcee05347abff51a4b854c9f04a8382bfff
David Choi <dharric@live.com>
2024-04-08 11:55:55 -0400

    fix: scheduler-utils locate.js issue in locateWith function

--- scheduler-utils/src/locate.js
+++ scheduler-utils/src/locate.js
@@ -1,4 +1,10 @@
-export function locateWith ({ loadProcessScheduler, loadScheduler, cache, followRedirects, checkForRedirect }) {
+export function locateWith({
+  loadProcessScheduler,
+  loadScheduler,
+  cache,
+  followRedirects,
+  checkForRedirect,
+}) {
   /**
    * Locate the scheduler for the given process.
    *
@@ -11,41 +17,48 @@ export function locateWith ({ loadProcessScheduler, loadScheduler, cache, follow
    * @returns {Promise<{ url: string, address: string }>} - an object whose url field is the Scheduler Location
    */
   return (process, schedulerHint) =>
-    cache.getByProcess(process)
-      .then(async (cached) => {
-        if (cached) return cached
+    cache.getByProcess(process).then(async (cached) => {
+      if (cached) return cached;
 
-        return Promise.resolve()
-          .then(async () => {
-            /**
-             * The the scheduler hint was provided,
-             * so skip querying the process and instead
-             * query the Scheduler-Location record directly
-             */
-            if (schedulerHint) {
-              const byOwner = await cache.getByOwner(schedulerHint)
-              if (byOwner) return (byOwner)
+      return Promise.resolve()
+        .then(async () => {
+          /**
+           * The the scheduler hint was provided,
+           * so skip querying the process and instead
+           * query the Scheduler-Location record directly
+           */
+          if (schedulerHint) {
+            const byOwner = await cache.getByOwner(schedulerHint);
+            if (byOwner) return byOwner;
 
-              return loadScheduler(schedulerHint).then((scheduler) => {
-                cache.setByOwner(scheduler.owner, scheduler.url, scheduler.ttl)
-                return scheduler
-              })
-            }
+            return loadScheduler(schedulerHint).then((scheduler) => {
+              cache.setByOwner(scheduler.owner, scheduler.url, scheduler.ttl);
+              return scheduler;
+            });
+          }
 
-            return loadProcessScheduler(process)
-          })
-          .then(async (scheduler) => {
-            let finalUrl = scheduler.url
-            /**
-             * If following redirects, then the initial request will be
-             * to a router. So we go hit the router and cache the
-             * redirected url for performance.
-             */
-            if (followRedirects) finalUrl = await checkForRedirect(scheduler.url, process)
+          return loadProcessScheduler(process);
+        })
+        .then(async (schedulerOrOwner) => {
+          if (
+            schedulerOrOwner.hasOwnProperty("url") &&
+            schedulerOrOwner.hasOwnProperty("address")
+          ) {
+            return schedulerOrOwner;
+          }
 
-            const byProcess = { url: finalUrl, address: scheduler.owner }
-            await cache.setByProcess(process, byProcess, scheduler.ttl)
-            return byProcess
-          })
-      })
+          let finalUrl = schedulerOrOwner.url;
+          /**
+           * If following redirects, then the initial request will be
+           * to a router. So we go hit the router and cache the
+           * redirected url for performance.
+           */
+          if (followRedirects)
+            finalUrl = await checkForRedirect(schedulerOrOwner.url, process);
+
+          const byProcess = { url: finalUrl, address: schedulerOrOwner.owner };
+          await cache.setByProcess(process, byProcess, schedulerOrOwner.ttl);
+          return byProcess;
+        });
+    });
 }
--- scheduler-utils/src/locate.test.js
+++ scheduler-utils/src/locate.test.js
@@ -1,179 +1,193 @@
-import { describe, test } from 'node:test'
-import * as assert from 'node:assert'
+import { describe, test } from "node:test";
+import * as assert from "node:assert";
 
-import { locateWith } from './locate.js'
+import { locateWith } from "./locate.js";
 
-const PROCESS = 'zc24Wpv_i6NNCEdxeKt7dcNrqL5w0hrShtSCcFGGL24'
-const SCHEDULER = 'gnVg6A6S8lfB10P38V7vOia52lEhTX3Uol8kbTGUT8w'
-const DOMAIN = 'https://foo.bar'
-const DOMAIN_REDIRECT = 'https://foo-redirect.bar'
-const TEN_MS = 10
+const PROCESS = "zc24Wpv_i6NNCEdxeKt7dcNrqL5w0hrShtSCcFGGL24";
+const SCHEDULER = "gnVg6A6S8lfB10P38V7vOia52lEhTX3Uol8kbTGUT8w";
+const DOMAIN = "https://foo.bar";
+const DOMAIN_REDIRECT = "https://foo-redirect.bar";
+const TEN_MS = 10;
 
-describe('locateWith', () => {
-  test('should load the value and cache it', async () => {
+describe("locateWith", () => {
+  test("should load the value and cache it", async () => {
     const locate = locateWith({
       loadProcessScheduler: async (process) => {
-        assert.equal(process, PROCESS)
-        return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER }
+        assert.equal(process, PROCESS);
+        return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER };
       },
-      loadScheduler: async () => assert.fail('should not load the scheduler if no hint'),
+      loadScheduler: async () =>
+        assert.fail("should not load the scheduler if no hint"),
       cache: {
         getByProcess: async (process) => {
-          assert.equal(process, PROCESS)
-          return undefined
+          assert.equal(process, PROCESS);
+          return undefined;
         },
         setByProcess: async (process, { url, address }, ttl) => {
-          assert.equal(process, PROCESS)
-          assert.equal(url, DOMAIN)
-          assert.equal(address, SCHEDULER)
-          assert.equal(ttl, TEN_MS)
+          assert.equal(process, PROCESS);
+          assert.equal(url, DOMAIN);
+          assert.equal(address, SCHEDULER);
+          assert.equal(ttl, TEN_MS);
         },
         setByOwner: async (owner, url, ttl) => {
-          assert.equal(owner, SCHEDULER)
-          assert.equal(url, DOMAIN)
-          assert.equal(ttl, TEN_MS)
-        }
+          assert.equal(owner, SCHEDULER);
+          assert.equal(url, DOMAIN);
+          assert.equal(ttl, TEN_MS);
+        },
       },
-      followRedirects: false
-    })
+      followRedirects: false,
+    });
 
-    await locate(PROCESS)
-      .then((res) => assert.deepStrictEqual(res, { url: DOMAIN, address: SCHEDULER }))
-  })
+    await locate(PROCESS).then((res) =>
+      assert.deepStrictEqual(res, { url: DOMAIN, address: SCHEDULER })
+    );
+  });
 
-  test('should serve the cached value', async () => {
+  test("should serve the cached value", async () => {
     const locate = locateWith({
       loadProcessScheduler: async () => {
-        assert.fail('should never call on chain if in cache')
+        assert.fail("should never call on chain if in cache");
       },
-      loadScheduler: async () => assert.fail('should not load the scheduler if no hint'),
+      loadScheduler: async () =>
+        assert.fail("should not load the scheduler if no hint"),
       cache: {
         getByProcess: async (process) => {
-          assert.equal(process, PROCESS)
-          return { url: DOMAIN, address: SCHEDULER }
+          assert.equal(process, PROCESS);
+          return { url: DOMAIN, address: SCHEDULER };
         },
-        getByOwner: async () => assert.fail('should not check cache by owner if cached by process'),
-        setByProcess: async () => assert.fail('should not set cache by process if cached by process'),
-        setByOwner: async () => assert.fail('should not set cache by owner if cached by process')
-      }
-    })
+        getByOwner: async () =>
+          assert.fail("should not check cache by owner if cached by process"),
+        setByProcess: async () =>
+          assert.fail("should not set cache by process if cached by process"),
+        setByOwner: async () =>
+          assert.fail("should not set cache by owner if cached by process"),
+      },
+    });
 
-    await locate(PROCESS)
-      .then((res) => assert.deepStrictEqual(res, { url: DOMAIN, address: SCHEDULER }))
-  })
+    await locate(PROCESS).then((res) =>
+      assert.deepStrictEqual(res, { url: DOMAIN, address: SCHEDULER })
+    );
+  });
 
-  test('should load the redirected value and cache it', async () => {
+  test("should load the redirected value and cache it", async () => {
     const locate = locateWith({
       loadProcessScheduler: async (process) => {
-        assert.equal(process, PROCESS)
-        return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER }
+        assert.equal(process, PROCESS);
+        return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER };
       },
-      loadScheduler: async () => assert.fail('should not load the scheduler if no hint'),
+      loadScheduler: async () =>
+        assert.fail("should not load the scheduler if no hint"),
       cache: {
         getByProcess: async (process) => {
-          assert.equal(process, PROCESS)
-          return undefined
+          assert.equal(process, PROCESS);
+          return undefined;
         },
         setByProcess: async (process, { url, address }, ttl) => {
-          assert.equal(process, PROCESS)
-          assert.equal(url, DOMAIN_REDIRECT)
-          assert.equal(address, SCHEDULER)
-          assert.equal(ttl, TEN_MS)
+          assert.equal(process, PROCESS);
+          assert.equal(url, DOMAIN_REDIRECT);
+          assert.equal(address, SCHEDULER);
+          assert.equal(ttl, TEN_MS);
         },
         setByOwner: async (owner, url, ttl) => {
-          assert.equal(owner, SCHEDULER)
+          assert.equal(owner, SCHEDULER);
           /**
            * Original DOMAIN not the redirect
            */
-          assert.equal(url, DOMAIN)
-          assert.equal(ttl, TEN_MS)
-        }
+          assert.equal(url, DOMAIN);
+          assert.equal(ttl, TEN_MS);
+        },
       },
       followRedirects: true,
       checkForRedirect: async (url, process) => {
-        assert.equal(process, PROCESS)
-        assert.equal(url, DOMAIN)
-        return DOMAIN_REDIRECT
-      }
-    })
+        assert.equal(process, PROCESS);
+        assert.equal(url, DOMAIN);
+        return DOMAIN_REDIRECT;
+      },
+    });
 
-    await locate(PROCESS)
-      .then((res) => assert.deepStrictEqual(res, { url: DOMAIN_REDIRECT, address: SCHEDULER }))
-  })
+    await locate(PROCESS).then((res) =>
+      assert.deepStrictEqual(res, { url: DOMAIN_REDIRECT, address: SCHEDULER })
+    );
+  });
 
-  test('should use the scheduler hint and skip querying for the process', async () => {
+  test("should use the scheduler hint and skip querying for the process", async () => {
     const locate = locateWith({
-      loadProcessScheduler: async () => assert.fail('should not load process if given a scheduler hint'),
+      loadProcessScheduler: async () =>
+        assert.fail("should not load process if given a scheduler hint"),
       loadScheduler: async (owner) => {
-        assert.equal(owner, SCHEDULER)
-        return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER }
+        assert.equal(owner, SCHEDULER);
+        return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER };
       },
       cache: {
         getByProcess: async (process) => {
-          assert.equal(process, PROCESS)
-          return undefined
+          assert.equal(process, PROCESS);
+          return undefined;
         },
         getByOwner: async (owner) => {
-          assert.equal(owner, SCHEDULER)
-          return undefined
+          assert.equal(owner, SCHEDULER);
+          return undefined;
         },
         setByProcess: async (process, { url, address }, ttl) => {
-          assert.equal(process, PROCESS)
-          assert.equal(url, DOMAIN_REDIRECT)
-          assert.equal(address, SCHEDULER)
-          assert.equal(ttl, TEN_MS)
+          assert.equal(process, PROCESS);
+          assert.equal(url, DOMAIN_REDIRECT);
+          assert.equal(address, SCHEDULER);
+          assert.equal(ttl, TEN_MS);
         },
         setByOwner: async (owner, url, ttl) => {
-          assert.equal(owner, SCHEDULER)
+          assert.equal(owner, SCHEDULER);
           /**
            * Original DOMAIN not the redirect
            */
-          assert.equal(url, DOMAIN)
-          assert.equal(ttl, TEN_MS)
-        }
+          assert.equal(url, DOMAIN);
+          assert.equal(ttl, TEN_MS);
+        },
       },
       followRedirects: true,
       checkForRedirect: async (url, process) => {
-        assert.equal(process, PROCESS)
-        assert.equal(url, DOMAIN)
-        return DOMAIN_REDIRECT
-      }
-    })
+        assert.equal(process, PROCESS);
+        assert.equal(url, DOMAIN);
+        return DOMAIN_REDIRECT;
+      },
+    });
 
-    await locate(PROCESS, SCHEDULER)
-      .then((res) => assert.deepStrictEqual(res, { url: DOMAIN_REDIRECT, address: SCHEDULER }))
-  })
+    await locate(PROCESS, SCHEDULER).then((res) =>
+      assert.deepStrictEqual(res, { url: DOMAIN_REDIRECT, address: SCHEDULER })
+    );
+  });
 
-  test('should use the scheduler hint and use the cached owner', async () => {
+  test("should use the scheduler hint and use the cached owner", async () => {
     const locate = locateWith({
-      loadProcessScheduler: async () => assert.fail('should not load process if given a scheduler hint'),
-      loadScheduler: async () => assert.fail('should not load the scheduler if cached'),
+      loadProcessScheduler: async () =>
+        assert.fail("should not load process if given a scheduler hint"),
+      loadScheduler: async () =>
+        assert.fail("should not load the scheduler if cached"),
       cache: {
         getByProcess: async (process) => {
-          assert.equal(process, PROCESS)
-          return undefined
+          assert.equal(process, PROCESS);
+          return undefined;
         },
         getByOwner: async (owner) => {
-          assert.equal(owner, SCHEDULER)
-          return { url: DOMAIN, ttl: TEN_MS, owner: SCHEDULER }
+          assert.equal(owner, SCHEDULER);
+          return { url: DOMAIN, address: SCHEDULER };
         },
         setByProcess: async (process, { url, address }, ttl) => {
-          assert.equal(process, PROCESS)
-          assert.equal(url, DOMAIN_REDIRECT)
-          assert.equal(address, SCHEDULER)
-          assert.equal(ttl, TEN_MS)
+          assert.equal(process, PROCESS);
+          assert.equal(url, DOMAIN_REDIRECT);
+          assert.equal(address, SCHEDULER);
         },
-        setByOwner: async () => assert.fail('should not cache by owner if cached')
+        setByOwner: async () =>
+          assert.fail("should not cache by owner if cached"),
       },
       followRedirects: true,
       checkForRedirect: async (url, process) => {
-        assert.equal(process, PROCESS)
-        assert.equal(url, DOMAIN)
-        return DOMAIN_REDIRECT
-      }
-    })
+        assert.equal(process, PROCESS);
+        assert.equal(url, DOMAIN);
+        return DOMAIN_REDIRECT;
+      },
+    });
 
-    await locate(PROCESS, SCHEDULER)
-      .then((res) => assert.deepStrictEqual(res, { url: DOMAIN_REDIRECT, address: SCHEDULER }))
-  })
-})
+    await locate(PROCESS, SCHEDULER).then((res) =>
+      assert.deepStrictEqual(res, { url: DOMAIN, address: SCHEDULER })
+    );
+  });
+});
