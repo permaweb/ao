831fdd30c540b0c0518ba0d7bc346407b3b84e28
Atticus <atticus@macbook36gb.local>
2024-04-23 11:06:38 -0600

    fix(signers): remove eth and sol signers and deps

--- connect/src/client/node/wallet.test.js
+++ connect/src/client/node/wallet.test.js
@@ -7,7 +7,7 @@ import { join } from 'node:path'
 
 import Arweave from 'arweave'
 
-import { createDataItemSigner, createEthereumDataItemSigner, createSolanaDataItemSigner } from './wallet.js'
+import { createDataItemSigner } from './wallet.js'
 
 describe('node - wallet', () => {
   /**
@@ -29,42 +29,6 @@ describe('node - wallet', () => {
       const wallet = JSON.parse(readFileSync(tmpWallet).toString())
 
       const signDataItem = createDataItemSigner(wallet)
-
-      const res = await signDataItem({
-        data: 'foobar',
-        tags: [{ name: 'foo', value: 'bar' }],
-        target: 'xwOgX-MmqN5_-Ny_zNu2A8o-PnTGsoRb_3FrtiMAkuw',
-        anchor: randomBytes(32)
-      })
-
-      console.log('signedDataItem', res)
-      assert.ok(res.id)
-      assert.ok(res.raw)
-    })
-
-    test('should create and sign the data item with Ethereum signer', async () => {
-      // const wallet = ethers.Wallet.createRandom().privateKey
-      const wallet = '0x5cb525ddd4b766761a10526dadfa84816f1ff272ba52c29ae55b1c8ac734984b'
-
-      const signDataItem = createEthereumDataItemSigner(wallet)
-
-      const res = await signDataItem({
-        data: 'foobar',
-        tags: [{ name: 'foo', value: 'bar' }],
-        target: 'xwOgX-MmqN5_-Ny_zNu2A8o-PnTGsoRb_3FrtiMAkuw',
-        anchor: randomBytes(32)
-      })
-
-      console.log('signedDataItem', res)
-      assert.ok(res.id)
-      assert.ok(res.raw)
-    })
-
-    test('should create and sign the data item with Solana signer', async () => {
-      // const wallet = b58.encode(SolanaWeb3.Keypair.generate().secretKey)
-      const wallet = '37ku4b5nwE2KBxoMvvkUV8nhcGQj8hdfGcLcv81tEbQhopLnccPwsxJGZdYasyCjbBrZsaAaiCVaC2EJfSbSib6p'
-
-      const signDataItem = createSolanaDataItemSigner(wallet)
 
       const res = await signDataItem({
         data: 'foobar',
