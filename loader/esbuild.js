8b6ca36af1d6134e21500cc1362c7684331bdbb9
Tom Wilson <tom@hyper.io>
2024-05-10 21:31:08 +0000

    feat(loader): trying some different emcc flags to improve maintenance experience

--- loader/esbuild.js
+++ loader/esbuild.js
@@ -1,9 +1,9 @@
 import * as esbuild from 'esbuild'
 
 await esbuild.build({
-  entryPoints: ['src/index.cjs'],
+  entryPoints: ['src/index2.js'],
   platform: 'node',
-  format: 'cjs',
+  format: 'esm',
   bundle: true,
-  outfile: 'dist/index.cjs'
+  outfile: 'dist/index.js'
 })
