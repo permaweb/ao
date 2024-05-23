e21ddad02e0071a1a23fc5e26132b80ca29b8a94
Vince Juliano <v.juliano6363@gmail.com>
2024-05-23 17:45:14 -0400

    chore(mu): remove cranking files #661

--- servers/mu/src/domain/lib/crank.js
+++
@@ -1,9 +0,0 @@
-import { of, fromPromise } from 'hyper-async'
-
-export function crankWith ({ logger, enqueuResults }) {
-  return (ctx) => {
-    return of(ctx)
-      .chain(fromPromise(enqueuResults))
-      .map(logger.tap('Cranked msgs'))
-  }
-}
