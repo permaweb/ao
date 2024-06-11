caa60e11f7cb1ad6d47b9a0b1aad247fbdbe8d9b
Tyler Hall <tyler@hyper.io>
2024-06-11 18:51:04 +0000

    feat(ur): more evenly distributed routing across hosts

--- servers/ur/src/domain.js
+++ servers/ur/src/domain.js
@@ -73,11 +73,14 @@ export function determineHostWith ({ hosts = [], bailout }) {
     }
 
     /**
-       * Check cache, and hydrate if necessary
-       */
+     * Check cache, and hydrate if necessary
+     */
     let hashSum = cache.get(processId)
     if (!hashSum) {
-      hashSum = computeHashSumFromProcessId(processId)
+      /**
+       * Only perform the expensive computation of hash -> idx once and cache
+       */
+      hashSum = computeHashSumFromProcessId({ processId, length: hosts.length })
       cache.set(processId, hashSum)
     }
 
@@ -85,6 +88,27 @@ export function determineHostWith ({ hosts = [], bailout }) {
   }
 }
 
-export function computeHashSumFromProcessId (processId) {
-  return processId.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)
+export function computeHashSumFromProcessId ({ processId, length }) {
+  // return processId.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0)
+  return Number(BigInt(cyrb53(processId)) % BigInt(length))
+}
+
+/**
+  cyrb53 (c) 2018 bryc (github.com/bryc)
+  License: Public domain (or MIT if needed). Attribution appreciated.
+  A fast and simple 53-bit string hash function with decent collision resistance.
+  Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
+*/
+const cyrb53 = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed; let h2 = 0x41c6ce57 ^ seed
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i)
+    h1 = Math.imul(h1 ^ ch, 2654435761)
+    h2 = Math.imul(h2 ^ ch, 1597334677)
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507)
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909)
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507)
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909)
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0)
 }
