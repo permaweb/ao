add57ba031fbe3013b122005ed33abf6732d3f63
Tyler Hall <tyler@hyper.io>
2024-06-25 15:15:03 +0000

    perf(cu): set fetch global dispatcher to keepAlive and share underlying tcp connection

--- servers/cu/src/routes/middleware/withDomain.js
+++ servers/cu/src/routes/middleware/withDomain.js
@@ -1,6 +1,15 @@
+import { fetch, setGlobalDispatcher, Agent } from 'undici'
+
 import { config } from '../../config.js'
 import { logger } from '../../logger.js'
 import { createApis, domainConfigSchema } from '../../domain/index.js'
+
+setGlobalDispatcher(new Agent({
+  /** the timeout, in milliseconds, after which a socket without active requests will time out. Monitors time between activity on a connected socket. This value may be overridden by *keep-alive* hints from the server */
+  keepAliveTimeout: 30 * 1000, // 30 seconds
+  /** the maximum allowed `idleTimeout`, in milliseconds, when overridden by *keep-alive* hints from the server. */
+  keepAliveMaxTimeout: 10 * 60 * 1000 // 10 mins
+}))
 
 /**
  * TODO: probably a better place to expose this,
