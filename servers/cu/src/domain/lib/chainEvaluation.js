e9af4ebd237c6beed2b1df47b9921d8c7f09377b
Tyler Hall <tyler@hyper.io>
2024-07-04 17:55:09 +0000

    perf(cu): wrap chainEvaluation with lock to prevent duplicate work #844

--- servers/cu/src/domain/lib/chainEvaluation.js
+++ servers/cu/src/domain/lib/chainEvaluation.js
@@ -1,5 +1,6 @@
 import { Rejected, Resolved, fromPromise, of } from 'hyper-async'
 import { identity, join, pick } from 'ramda'
+import AsyncLock from 'async-lock'
 
 import { findEvaluationSchema } from '../dal.js'
 import { findPendingForProcessBeforeWith, isLaterThan } from '../utils.js'
@@ -9,6 +10,8 @@ import { findPendingForProcessBeforeWith, isLaterThan } from '../utils.js'
  *
  * If another request comes in to invoke a readState that is already
  * pending, then we will just return that Async instead of spinning up a new readState.
+ *
+ * TODO: this should perhaps be moved to the top of readState and injected into chainEvaluation
  *
  * @type {Map<string, { startTime: Date, pending: Promise<any> }}
  */
@@ -22,6 +25,8 @@ const removePendingReadState = (key) => (res) => {
 export const pendingReadStates = () => Object.fromEntries(pendingReadState.entries())
 
 const findPendingForProcessBefore = findPendingForProcessBeforeWith(pendingReadState)
+
+const chainLock = new AsyncLock({ maxPending: Number.MAX_SAFE_INTEGER })
 
 function loadLatestEvaluationWith ({ findEvaluation, findLatestProcessMemory, logger }) {
   findEvaluation = fromPromise(findEvaluationSchema.implement(findEvaluation))
@@ -116,23 +121,6 @@ function loadLatestEvaluationWith ({ findEvaluation, findLatestProcessMemory, lo
     .bichain(maybeCachedMemory, Resolved)
 }
 
-/**
- * @typedef Args
- * @property {string} id - the id of the process
- *
- * @typedef Result
- * @property {string} id - the id of the process
- * @property {string} owner
- * @property {any} tags
- * @property {{ height: number, timestamp: number }} block
- *
- * @callback LoadProcess
- * @param {Args} args
- * @returns {Async<Result>}
- *
- * @param {Env} env
- * @returns {LoadProcess}
- */
 export function chainEvaluationWith (env) {
   const logger = env.logger.child('chainEvaluation')
   env = { ...env, logger }
@@ -153,148 +141,163 @@ export function chainEvaluationWith (env) {
   return ({ processId, to, ordinate, cron, exact, needsMemory, next }) => {
     const key = pendingKey([processId, to, ordinate, cron, exact, needsMemory])
 
-    return of(key)
-      .chain((key) => {
-        /**
-         * Already pending evaluation, so just return its eval stream
-         */
-        if (pendingReadState.has(key)) return of(key).chain(fromPromise((key) => pendingReadState.get(key).pending))
-
-        const pendingForProcessBefore = findPendingForProcessBefore({ processId, timestamp: to })
-        /**
-          * Check if the starting point found in caching layers
-          * is earlier than a pending eval stream, and simply
-          * chain off the pending eval stream if so.
-          *
-          * Otherwise, we start a separate evaluation stream using the
-          * starting point from the cache.
-          *
-          * This will help ensure a request to evaluate an old message
-          * does not cause delays on evaluations of newer messages that may
-          * have later checkpoints available on chain to "hot start" from.
-          *
-          * By default, there is nothing to chain off of, so just resolve immediately
-          */
-        let newEntry = {
-          startTime: new Date(),
-          chainedTo: undefined,
-          pending: Promise.resolve()
-        }
-
-        /**
-         * No pending readState, so no reason to check cache in order
-         * to compare to a pending readState
-         */
-        if (!pendingForProcessBefore) return Resolved(newEntry)
-
-        return loadLatestEvaluation({ id: processId, to, ordinate, cron, exact, needsMemory })
-          .chain((res) => {
+    return of()
+      /**
+       * LOCK CRITICAL SECTION:
+       *
+       * determine what to do with the incoming readState:
+       * - chain it to an existing evaluation stream
+       * - dedupe it, due to an identicial evaluation stream already executing
+       * - start it up as a brand new evaluation stream
+       *
+       * This check requires acquisition of a lock, to avoid race conditions from multiple
+       * incoming readStates asynchronously determining whether or not to chain
+       */
+      .chain(fromPromise(() => chainLock.acquire(processId, async () =>
+        Promise.resolve(key)
+          .then(async (key) => {
             /**
-              * The exact evaluation (identified by its input messages timestamp)
-              * was found in the cache, so just return it.
-              *
-              * evaluate sets output below, so since we've found the output
-              * without evaluating, we simply set output to the result of the cached
-              * evaluation.
-              */
-            if (res.exact) return Resolved({ ...res, output: res.result })
-
-            /**
-             * Often times, multiple requests will be sent to eval the same message,
-             * So before we duplicate work, we double-check if there is a pending readState.
-             *
-             * A bit clunky, but helps prevent duplicate work, due to both requests coming
-             * in before the process could be loaded
+             * There is already an exact instance of the readState already pending,
+             * so simply return the entry, and no new work need to be performed
              */
-            if (pendingReadState.has(key)) return of(key).chain(fromPromise((key) => pendingReadState.get(key).pending))
-
-            const [pendingKey, { pending, chainedTo }] = pendingForProcessBefore
-            const [, pTo, pOrdinate, pCron] = pendingKey.split(',')
+            if (pendingReadState.has(key)) return [false, pendingReadState.get(key)]
 
             /**
-             * If the incoming eval stream will be cold started,
-             * then we chain it to ANY pending eval stream. This is to
-             * prevent multiple cold starts of the same process. This could mean that, in some cases,
-             * a message eval from a cold start could "wait" unnecessarily longer (ie. the pending is evaluating
-             * up to a much later message than the one being requested). To that, there are two points:
+             * Check if the starting point found in caching layers
+             * is earlier than a pending eval stream, and simply
+             * chain off the pending eval stream if so.
              *
-             * 1. the request to eval an old message ought to be dispreferred anyway
-             * 2. the old message will be in the CUs result cache, and so immediately
-             * be returned to the client, without starting another eval stream, a boon for the CU.
+             * Otherwise, we start a separate evaluation stream using the
+             * starting point from the cache.
              *
-             * Regardless, this branch should only trigger when there is a process with a lengthy eval stream from a cold start,
-             * and most of the time cold starts happen only for new processes, whose message history is short, and typically cheaper
-             * to compute.
+             * This will help ensure a request to evaluate an old message
+             * does not cause delays on evaluations of newer messages that may
+             * have later checkpoints available on chain to "hot start" from.
              *
-             * ---
-             *
-             * Alternatively (not a cold start), we must compare the actual chronology
-             * between the loaded latest evaluation and the found
-             * pending readState, and decide whether it is advantageous to chain
+             * By default, there is nothing to chain off of, so just resolve immediately
              */
-            const isPendingLaterThanCached = res.isColdStart || (!!res.from && isLaterThan(
-              { timestamp: res.from, ordinate: res.ordinate, cron: res.fromCron },
-              { timestamp: pTo, ordinate: pOrdinate, cron: pCron }
-            ))
-
-            if (isPendingLaterThanCached) {
-              env.logger(
-                'found pending readState at "%j" to chain to, later than cached %j, for incoming readState "%s"',
-                { key: pendingKey, chainedTo },
-                !res.isColdStart
-                  ? { timestamp: res.from, ordinate: res.ordinate, cron: res.fromCron }
-                  : { COLD_START: true },
-                key
-              )
-              newEntry = {
-                startTime: new Date(),
-                chainedTo: pendingKey,
-                /**
-                  * chain off already pending readState
-                  * @type {Promise}
-                  */
-                pending
-              }
+            let newEntry = {
+              startTime: new Date(),
+              chainedTo: undefined,
+              pending: Promise.resolve()
             }
 
-            return Resolved(newEntry)
-          })
-      })
-      .chain((newEntry) => {
-        /**
-         * There's no readState needed to be performed,
-         * chained or otherwise, either because an exact match
-         * was found, or a existing readState for the same message
-         * exists.
-         *
-         * So bail out early
-         *
-         * TODO: could probably make cleaner upstream
-         */
-        if (newEntry.exact || newEntry.output) return Resolved(newEntry)
-
-        /**
-         * Add the entry to the pending readStates
-         */
-        pendingReadState.set(key, {
-          startTime: newEntry.startTime,
-          chainedTo: newEntry.chainedTo,
-          pending: newEntry.pending
+            const pendingForProcessBefore = findPendingForProcessBefore({ processId, timestamp: to })
             /**
-              * Unwrapping the async here prevents duplicate work,
-              *
-              * It is still wrapped in an Async below, but it's the same
-              * underlying Promise, so no duplicate work
+             * No pending evaluation stream was found, so there is no reason to check the process
+             * memory cache for an entry and then to compare it to the pending evalaution stream,
+             * in order to determine the optimal chaining strategy.
+             *
+             * This will result in an evaluation stream being immediately spun up
+             * with no chaining
              */
-            .then(() => next.toPromise())
-            .finally(removePendingReadState(key))
-        })
+            if (!pendingForProcessBefore) return [true, newEntry]
 
-        /**
-         * Return an Async wrapping the actual promise, so minimal
-         * duplicate work is performed
-         */
-        return of(key).chain(fromPromise((key) => pendingReadState.get(key).pending))
-      })
+            return loadLatestEvaluation({ id: processId, to, ordinate, cron, exact, needsMemory })
+              .toPromise()
+              .then((res) => {
+                /**
+                 * The exact evaluation (identified by its input messages timestamp)
+                 * was found in the cache, so just return it.
+                 *
+                 * This results in no new evaluation stream being performed, as the work
+                 * has already been performed and cached.
+                 *
+                 * An evaluation stream will ultimately return a { result } containing
+                 * the message result. so since we've found the output
+                 * cached, we simply set { result } to the cached
+                 * evaluation output, to match the shape.
+                 */
+                if (res.exact) return [false, { pending: Promise.resolve({ ...res, output: res.result }) }]
+
+                const [pendingKey, { pending, chainedTo }] = pendingForProcessBefore
+                const [, pTo, pOrdinate, pCron] = pendingKey.split(',')
+
+                /**
+                 * If the incoming eval stream will be cold started,
+                 * then we chain it to ANY pending eval stream. This is to
+                 * prevent multiple cold starts of the same process. This could mean that, in some cases,
+                 * a message eval from a cold start could "wait" unnecessarily longer (ie. the pending is evaluating
+                 * up to a much later message than the one being requested). To that, there are two points:
+                 *
+                 * 1. the request to eval an old message ought to be dispreferred anyway
+                 * 2. the old message will be in the CUs result cache, and so immediately
+                 * be returned to the client, without starting another eval stream, a boon for the CU.
+                 *
+                 * Regardless, this branch should only trigger when there is a process with a lengthy eval stream from a cold start,
+                 * and most of the time cold starts happen only for new processes, whose message history is short, and typically cheaper
+                 * to compute.
+                 *
+                 * ---
+                 *
+                 * Alternatively (not a cold start), we must compare the actual chronology
+                 * between the loaded latest evaluation and the found
+                 * pending readState, and decide whether it is advantageous to chain
+                 */
+                const isPendingLaterThanCached = res.isColdStart || (!!res.from && isLaterThan(
+                  { timestamp: res.from, ordinate: res.ordinate, cron: res.fromCron },
+                  { timestamp: pTo, ordinate: pOrdinate, cron: pCron }
+                ))
+
+                if (isPendingLaterThanCached) {
+                  env.logger(
+                    'found pending readState at "%j" to chain to, later than cached %j, for incoming readState "%s"',
+                    { key: pendingKey, chainedTo },
+                    !res.isColdStart
+                      ? { timestamp: res.from, ordinate: res.ordinate, cron: res.fromCron }
+                      : { COLD_START: true },
+                    key
+                  )
+                  /**
+                   * chain off already pending evaluation stream,
+                   * indicating it's place in the chain by the
+                   * entry's "chainedTo" field.
+                   *
+                   * The result is a set of trees where each pending evaluation stream
+                   * may have a parent evaluation stream that it is waiting for to complete
+                   * and potentially many children evaluation streams itself
+                   */
+                  newEntry = {
+                    startTime: new Date(),
+                    chainedTo: pendingKey,
+                    /**
+                     * chain off already pending readState
+                     * @type {Promise}
+                     */
+                    pending
+                  }
+                }
+
+                return [true, newEntry]
+              })
+          })
+          .then(([isNewEntry, res]) => {
+            /**
+             * A cached result or an exact pending readState was found
+             * so just return it
+             */
+            if (!isNewEntry) return res
+
+            /**
+             * New evaluations must be performed, so place it
+             * in the map of pendingReadStates, and chain the new work to be performed
+             * off of the pending work, then clean up by removing itself from the map
+             */
+            const newEntry = {
+              startTime: res.startTime,
+              chainedTo: res.chainedTo,
+              pending: res.pending
+                .then(() => next.toPromise())
+                .finally(removePendingReadState(key))
+            }
+            pendingReadState.set(key, newEntry)
+
+            return newEntry
+          })
+      )))
+      /**
+       * LOCK RELEASED
+       */
+      .chain(fromPromise((res) => res.pending))
   }
 }
