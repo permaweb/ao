8527c85750c029838c06a965670152e7d9e5c053
Jack Frain <jfrain99@gmail.com>
2024-07-26 11:23:50 -0400

    fix(mu): stop queue size metric from becoming negative

--- servers/mu/src/domain/clients/worker.js
+++ servers/mu/src/domain/clients/worker.js
@@ -147,26 +147,14 @@ function processResultsWith ({ enqueue, dequeue, processResult, logger, TASK_QUE
   }
 }
 
-function broadcastEnqueueWith ({ enqueue }) {
+function broadcastEnqueueWith ({ enqueue, queue }) {
   return (...args) => {
     broadcastChannel.postMessage({
-      purpose: 'metrics',
-      action: 'enqueue'
+      purpose: 'queue-size',
+      size: queue.length + 1,
+      time: Date.now()
     })
     return enqueue(...args)
-  }
-}
-
-function broadcastDequeueWith ({ dequeue }) {
-  return (...args) => {
-    const dequeuedResult = dequeue(...args)
-    if (dequeuedResult) {
-      broadcastChannel.postMessage({
-        purpose: 'metrics',
-        action: 'dequeue'
-      })
-    }
-    return dequeuedResult
   }
 }
 
@@ -178,6 +166,14 @@ const queue = await createTaskQueue({
 })
 
 /**
+ * We post a message with the queue size every second to ensure
+ * that the sliding window array of queue sizes does not become
+ * stale with minutes-old values.
+ */
+setInterval(() => {
+  broadcastChannel.postMessage({ purpose: 'queue-size', size: queue.length, time: Date.now() })
+}, 1000)
+/**
  * Initialize a set of task ids.
  * These task ids represent database ids
  * to be remove on the next cron cycle.
@@ -185,9 +181,8 @@ const queue = await createTaskQueue({
 const dequeuedTasks = new Set()
 
 const enqueue = enqueueWith({ queue, queueId: workerData.queueId, logger, db })
-const broadcastEnqueue = broadcastEnqueueWith({ enqueue })
+const broadcastEnqueue = broadcastEnqueueWith({ enqueue, queue })
 const dequeue = dequeueWith({ queue, logger, dequeuedTasks })
-const broadcastDequeue = broadcastDequeueWith({ dequeue })
 const removeDequeuedTasks = removeDequeuedTasksWith({ dequeuedTasks, queueId: workerData.queueId, db })
 
 const enqueueResults = enqueueResultsWith({
@@ -204,7 +199,7 @@ const processResult = processResultWith({
 
 const processResults = processResultsWith({
   enqueue: broadcastEnqueue,
-  dequeue: broadcastDequeue,
+  dequeue,
   processResult,
   logger,
   TASK_QUEUE_MAX_RETRIES: workerData.TASK_QUEUE_MAX_RETRIES,
--- servers/mu/src/domain/lib/handle-worker-queue-message.js
+++ servers/mu/src/domain/lib/handle-worker-queue-message.js
@@ -1,16 +1,37 @@
-function metricsMessageHandler (payload, gauge) {
-  if (payload.action === 'enqueue') {
-    gauge.inc()
-  }
-  if (payload.action === 'dequeue') {
-    gauge.dec()
+function metricsMessageHandler (payload, maximumQueueArray, maximumQueueTimeArray) {
+  const { size, time } = payload
+  const seconds = Math.floor(time / 1000 % 60)
+
+  const currSize = maximumQueueArray[seconds]
+  const currTime = maximumQueueTimeArray[seconds]
+  if (!currSize) {
+  /**
+   * If the array is empty at this seconds mark,
+   * set the value to our data.
+   */
+    maximumQueueArray[seconds] = size
+    maximumQueueTimeArray[seconds] = time
+  } else if (Math.abs(time - currTime) > 1000) {
+  /**
+   * If the array is not empty but the time value is more than a second old
+   * (ie., the value is from a minute+ ago), set the value to our data
+   */
+    maximumQueueArray[seconds] = size
+    maximumQueueTimeArray[seconds] = time
+  } else if (size > currSize) {
+  /**
+   * If the array has a value within this second, replace it if
+   * our data is the maximum
+   */
+    maximumQueueArray[seconds] = size
+    maximumQueueTimeArray[seconds] = time
   }
 }
 
-export function handleWorkerQueueMessage ({ queueGauge }) {
+export function handleWorkerQueueMessage ({ queueGauge, maximumQueueArray, maximumQueueTimeArray }) {
   return ({ payload }) => {
-    if (payload.purpose === 'metrics') {
-      metricsMessageHandler(payload, queueGauge)
+    if (payload.purpose === 'queue-size') {
+      metricsMessageHandler(payload, maximumQueueArray, maximumQueueTimeArray)
     }
   }
 }
