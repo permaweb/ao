aa1ad02686994f987bad8efed1a26a3a5e90cbf0
Jack Frain <jfrain99@gmail.com>
2024-07-30 12:07:34 -0400

    perf(scheduler-utils): add exponential backoff to gateway requests

--- scheduler-utils/src/utils.js
+++ scheduler-utils/src/utils.js
@@ -2,3 +2,56 @@ export function trimSlash (str = '') {
   str = str.trim()
   return str.endsWith('/') ? trimSlash(str.slice(0, -1)) : str
 }
+
+/**
+ * A function that, given a function, will immediately invoke it,
+ * then retry it on errors, using an exponential backoff.
+ *
+ * If the final retry fails, then the overall Promise is rejected
+ * with that error
+ *
+ * @param {function} fn - the function to be called
+ * @param {{ maxRetries: number, delay: number }} param1 - the number of total retries and increased delay for each try
+ */
+export const backoff = (
+  fn,
+  { maxRetries = 0, delay = 300 }
+) => {
+  /**
+   * Recursive function that recurses with exponential backoff
+   */
+  const action = (retry, delay) => {
+    return Promise.resolve()
+      .then(fn)
+      .catch((err) => {
+        // Reached max number of retries
+        if (retry >= maxRetries) {
+          return Promise.reject(err)
+        }
+
+        /**
+         * increment the retry count Retry with an exponential backoff
+         */
+        const newRetry = retry + 1
+        const newDelay = delay + delay
+        /**
+         * Retry in {delay} milliseconds
+         */
+        return new Promise((resolve) => setTimeout(resolve, delay))
+          .then(() => action(newRetry, newDelay))
+      })
+  }
+
+  return action(0, delay)
+}
+
+/**
+ * Checks if a response is OK. Otherwise, throw response.
+ *
+ * @param {Response} res - The response to check
+ * @returns
+ */
+export const okRes = (res) => {
+  if (res.ok) return res
+  throw res
+}
--- scheduler-utils/src/utils.test.js
+++ scheduler-utils/src/utils.test.js
@@ -1,6 +1,6 @@
 import { describe, test } from 'node:test'
 import * as assert from 'node:assert'
-import { trimSlash } from './utils.js'
+import { backoff, trimSlash } from './utils.js'
 
 describe('trimSlash', () => {
   test('should remove trailing slash from url', () => {
@@ -8,5 +8,77 @@ describe('trimSlash', () => {
     assert.equal(resultWithTrailingSlash, 'https://foo.bar')
     const resultWithoutTrailingSlash = trimSlash('https://foo.bar')
     assert.equal(resultWithoutTrailingSlash, 'https://foo.bar')
+  })
+})
+
+describe('backoff', () => {
+  function isPromise (obj) {
+    return (
+      !!obj &&
+      (typeof obj === 'object' || typeof obj === 'function') &&
+      typeof obj.then === 'function'
+    )
+  }
+
+  test('should return a promise', () => {
+    assert.ok(isPromise(
+      backoff(() => Promise.resolve(''), {
+        maxRetries: 0,
+        delay: 0
+      })
+    ))
+
+    assert.ok(isPromise(
+      backoff(() => '', {
+        maxRetries: 0,
+        delay: 0
+      })
+    ))
+  })
+
+  test('should not retry calling the function', async () => {
+    let count = 0
+    const fn = () => count++
+    await backoff(fn, {
+      maxRetries: 0,
+      delay: 0
+    })
+
+    assert.equal(count, 1)
+  })
+
+  test('should retry calling the function', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+      return count
+        ? Promise.resolve('foo')
+        // eslint-disable-next-line
+        : Promise.reject('bar')
+    }
+
+    const res = await backoff(fn, {
+      maxRetries: 1,
+      delay: 0
+    })
+
+    assert.equal(res, 'foo')
+  })
+
+  test('should bubble the error if all retries are unsuccessful', async () => {
+    let count = 0
+    const fn = () => {
+      count++
+      // eslint-disable-next-line
+      return Promise.reject('bar')
+    }
+
+    await backoff(fn, {
+      maxRetries: 2,
+      delay: 0
+    }).catch(err => {
+      assert.equal(err, 'bar')
+      assert.equal(count, 3)
+    })
   })
 })
