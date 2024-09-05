974e1c73359eb0085fd8e7d2d1916f7109cfaf5c
av1ctor <av1ctortv@gmail.com>
2024-09-05 12:27:30 -0300

    fix(dev-cli): added missing Memory-Limit and Compute-Limit tags, updated Module-Format to wasm64

--- dev-cli/container/src/node/src/defaults.js
+++ dev-cli/container/src/node/src/defaults.js
@@ -1,7 +1,9 @@
-export const DEFAULT_MODULE_FORMAT_TAG = { name: 'Module-Format', value: 'wasm32-unknown-emscripten' }
+export const DEFAULT_MODULE_FORMAT_TAG = { name: 'Module-Format', value: 'wasm64-unknown-emscripten-draft_2024_02_15' }
 export const DEFAULT_INPUT_ENCODING_TAG = { name: 'Input-Encoding', value: 'JSON-1' }
 export const DEFAULT_OUTPUT_ENCODING_TAG = { name: 'Output-Encoding', value: 'JSON-1' }
 export const DEFAULT_VARIANT_TAG = { name: 'Variant', value: 'ao.TN.1' }
+export const DEFAULT_MEMORY_LIMIT_TAG = {name: 'Memory-Limit', value: '1-gb' }
+export const DEFAULT_COMPUTE_LIMIT_TAG = {name: 'Compute-Limit', value: '9000000000000' }
 
 export const DEFAULT_BUNDLER_HOST = 'https://up.arweave.net'
 
@@ -12,5 +14,7 @@ export const AoModuleTags = [
   DEFAULT_INPUT_ENCODING_TAG,
   DEFAULT_OUTPUT_ENCODING_TAG,
   DEFAULT_VARIANT_TAG,
+  DEFAULT_MEMORY_LIMIT_TAG,
+  DEFAULT_COMPUTE_LIMIT_TAG,
   { name: 'Content-Type', value: 'application/wasm' }
 ]
