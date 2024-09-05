314fad66f4cd5f5356e1fe4b92d4b595c59fb982
Tyler Hall <tyler@hyper.io>
2024-09-05 10:50:03 -0400

    chore(dev-cli): remove unsupported bundlers. Bump deps. Update defaults to align with 0.1 cli build

--- dev-cli/container/src/node/src/defaults.js
+++ dev-cli/container/src/node/src/defaults.js
@@ -1,4 +1,4 @@
-export const DEFAULT_MODULE_FORMAT_TAG = { name: 'Module-Format', value: 'wasm32-unknown-emscripten' }
+export const DEFAULT_MODULE_FORMAT_TAG = { name: 'Module-Format', value: 'wasm64-unknown-emscripten-draft_2024_02_15' }
 export const DEFAULT_INPUT_ENCODING_TAG = { name: 'Input-Encoding', value: 'JSON-1' }
 export const DEFAULT_OUTPUT_ENCODING_TAG = { name: 'Output-Encoding', value: 'JSON-1' }
 export const DEFAULT_VARIANT_TAG = { name: 'Variant', value: 'ao.TN.1' }
