f507c0e49fc7eaed3dfbcc9b57579816c2e475ef
Vlad Kutsenko <vladdie@gmail.com>
2024-10-28 13:46:33 -0400

    enabling debug logging for the WASM build command

--- dev-cli/src/commands/build.js
+++ dev-cli/src/commands/build.js
@@ -4,20 +4,36 @@ import { Command } from '../deps.js'
 import { VERSION } from '../versions.js'
 
 export async function build () {
+  console.log(`Building the module using ao tool version ${VERSION.CLI}, docker image version: ${VERSION.IMAGE}`);
   const pwd = Deno.cwd()
+  const cmd = [
+    'docker',
+    'run',
+    '--platform',
+    'linux/amd64',
+    '-v',
+    `${pwd}:/src`,
+    '--env', 
+    'DEBUG=True',
+    `p3rmaw3b/ao:${VERSION.IMAGE}`,
+    'ao-build-module'
+  ];
+  console.log(`Docker cmd: ${cmd}`);
   const p = Deno.run({
-    cmd: [
-      'docker',
-      'run',
-      '--platform',
-      'linux/amd64',
-      '-v',
-      `${pwd}:/src`,
-      `p3rmaw3b/ao:${VERSION.IMAGE}`,
-      'ao-build-module'
-    ]
+    cmd,
+    // stdout: "piped",
+    // stderr: "piped"
   })
+
   await p.status()
+
+  // const output = await p.output() // "piped" must be set
+  // const outStr = new TextDecoder().decode(output);
+  // console.log('Build run stdout', outStr);
+
+  // const error = await p.stderrOutput();
+  // const errorStr = new TextDecoder().decode(error); 
+  // console.log('Build run stderr', errorStr);
 }
 
 export const command = new Command()
