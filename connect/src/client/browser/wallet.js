5f97b93b558f8b617f371b9ddcf97b42e0337502
Tyler Hall <tyler@hyper.io>
2025-02-01 01:15:28 -0500

    feat(connect): add connect.hb() to use HyperBEAM mode

--- connect/src/client/browser/wallet.js
+++ connect/src/client/browser/wallet.js
@@ -1,12 +1,15 @@
 import { Buffer } from 'buffer/index.js'
 import * as WarpArBundles from 'warp-arbundles'
+import { httpbis } from 'http-message-signatures'
 
 // eslint-disable-next-line no-unused-vars
 import { Types } from '../../dal.js'
+import { httpSigName } from '../hb.js'
 
 if (!globalThis.Buffer) globalThis.Buffer = Buffer
 
 const { DataItem } = WarpArBundles
+const { signMessage } = httpbis
 
 /**
  * A function that builds a signer using the global arweaveWallet
@@ -19,7 +22,6 @@ const { DataItem } = WarpArBundles
  * @returns {Types['signer']} - The signer function
  * @example
  * const signer = createDataItemSigner(window.arweaveWallet)
- *
  */
 export function createDataItemSigner (arweaveWallet) {
   /**
@@ -39,6 +41,47 @@ export function createDataItemSigner (arweaveWallet) {
       raw: await dataItem.getRaw()
     }
   }
+
+  return signer
+}
+
+export function createHbSigner (arweaveWallet) {
+  const params = ['alg', 'keyid'].sort()
+
+  let permsP
+  const signer = async ({ fields, request }) => new Promise((resolve) => {
+    if (!permsP) {
+      permsP = arweaveWallet.connect([
+        'ACCESS_ADDRESS',
+        'ACCESS_PUBLIC_KEY',
+        'SIGNATURE'
+      ]).then(async () => {
+        const publicKey = await arweaveWallet.getActivePublicKey()
+        const address = await arweaveWallet.getActiveAddress()
+        return { publicKey, address }
+      })
+    }
+
+    return resolve(permsP)
+  }).then(({ publicKey, address }) => {
+    return signMessage({
+      key: {
+        id: publicKey,
+        alg: 'rsa-pss-sha512',
+        sign: async (data) => {
+          const view = await arweaveWallet.signMessage(
+            data,
+            { hashAlgorithm: 'SHA-512' }
+          )
+
+          return Buffer.from(view)
+        }
+      },
+      fields,
+      name: httpSigName(address),
+      params
+    }, request)
+  })
 
   return signer
 }
--- connect/src/index.js
+++ connect/src/index.js
@@ -1,4 +1,4 @@
-import { connect, serializeCron } from './index.common.js'
+import { connectWith, serializeCron } from './index.common.js'
 
 import { WalletClient } from './client/node/index.js'
 const GATEWAY_URL = process.env.GATEWAY_URL || undefined
@@ -8,7 +8,24 @@ const GRAPHQL_URL = process.env.GRAPHQL_URL || undefined
 const GRAPHQL_MAX_RETRIES = process.env.GRAPHQL_MAX_RETRIES || undefined
 const GRAPHQL_RETRY_BACKOFF = process.env.GRAPHQL_RETRY_BACKOFF || undefined
 
-const { result, results, message, spawn, monitor, unmonitor, dryrun, assign } = connect({ GATEWAY_URL, MU_URL, CU_URL, GRAPHQL_URL, GRAPHQL_MAX_RETRIES, GRAPHQL_RETRY_BACKOFF })
+const HB_URL = process.env.HB_URL || undefined
+
+const connect = connectWith({
+  createDataItemSigner: WalletClient.createDataItemSigner,
+  createHbSigner: WalletClient.createHbSigner
+})
+
+const { result, results, message, spawn, monitor, unmonitor, dryrun, assign, createDataItemSigner } = connect({
+  GATEWAY_URL,
+  MU_URL,
+  CU_URL,
+  GRAPHQL_URL,
+  GRAPHQL_MAX_RETRIES,
+  GRAPHQL_RETRY_BACKOFF
+})
+
+const originalHb = connect.hb
+connect.hb = ({ URL = HB_URL, ...rest }) => originalHb({ URL, ...rest })
 
 export { result, results, message, spawn, monitor, unmonitor, dryrun, assign }
 export { connect }
@@ -20,4 +37,4 @@ export { serializeCron }
  * This is provided as a convenience for consumers of the SDK
  * to use, but consumers can also implement their own signer
  */
-export const createDataItemSigner = WalletClient.createDataItemSigner
+export { createDataItemSigner }
