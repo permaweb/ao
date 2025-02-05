3d2de43d9853da106854c5690a1157e7fd55cfaf
Tyler Hall <tyler@hyper.io>
2025-02-05 22:44:55 +0000

    chore(cu): cu and hbu modes have all CU endpoints

--- servers/cu/src/effects/ao-http/routes/middleware/withCuMode.js
+++ servers/cu/src/effects/ao-http/routes/middleware/withCuMode.js
@@ -1,10 +1,10 @@
-const withUnitMode = (mode) => (handler) => (req, res, next) => {
+const withUnitMode = (modes) => (handler) => (req, res, next) => {
   const { UNIT_MODE } = req.config
 
-  if (UNIT_MODE !== mode) return res.status(404).send('Not Found')
+  if (!modes.includes(UNIT_MODE)) return res.status(404).send('Not Found')
 
   return handler(req, res, next)
 }
 
-export const withCuMode = withUnitMode('cu')
-export const withRuMode = withUnitMode('ru')
+export const withCuMode = withUnitMode(['cu', 'hbu'])
+export const withRuMode = withUnitMode(['ru'])
