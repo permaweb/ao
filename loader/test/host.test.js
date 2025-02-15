88d8289e424ea8e307d2e80dcbcd1724f08d9fe7
rakis <tom@hyper.io>
2025-02-15 11:28:11 -0500

    fix(loader): improved tests to add host tests

--- loader/test/host.test.js
+++ loader/test/host.test.js
@@ -3,10 +3,10 @@ import { test } from 'node:test'
 import * as assert from 'node:assert'
 
 // skip test first then add back next pr
-test.skip('genEnv should return nil', async () => {
+test('genEnv should return nil', async () => {
     const aos = await aoslocal()
 
-    // const result = await aos.eval('if not os.getenv("PATH") then print([[2]]) else print([[1]]) end')
-    const result = await aos.eval('print(os.getenv([[HOME]]))')
+    const result = await aos.eval('if not os.getenv("PATH") then print([[2]]) else print([[1]]) end')
+    // const result = await aos.eval('print(os.getenv([[PATH]]))')
     assert.equal(result.Output.data, '2')
 })
